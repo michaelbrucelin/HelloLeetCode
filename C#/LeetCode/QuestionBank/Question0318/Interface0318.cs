3ba30071c966765c040e76a0e4e38bb5c0f4b8d2
brucelin michael <michaelbrucelin@163.com>
2022-12-02 11:29:50 +0800

    20221202-1129

---
+++ C#/LeetCode/QuestionBank/Question0318/Interface0318.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0318
+{
+    public interface Interface0318
+    {
+        public int MaxProduct(string[] words);
+    }
+}
