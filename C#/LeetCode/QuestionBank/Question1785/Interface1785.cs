c92e68c5924184c146697b8b28cc017d2a75f123
brucelin michael <michaelbrucelin@163.com>
2022-12-16 13:26:26 +0800

    20221216-1326

---
+++ C#/LeetCode/QuestionBank/Question1785/Interface1785.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1785
+{
+    public interface Interface1785
+    {
+        public int MinElements(int[] nums, int limit, int goal);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1785/Solution1785.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1785
+{
+    public class Solution1785 : Interface1785
+    {
+        public int MinElements(int[] nums, int limit, int goal)
+        {
+            long distance = goal;  // 由题目知int不够用
+            for (int i = 0; i < nums.Length; i++) distance -= nums[i];
+            if (distance == 0) return 0;
+
+            if (distance < 0) distance *= -1;
+            return (int)((distance - 1) / limit) + 1;
+        }
+    }
+}
