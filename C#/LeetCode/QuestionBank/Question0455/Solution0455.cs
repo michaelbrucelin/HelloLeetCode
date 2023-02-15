f3c92a846e1e291daf2226eccfeab79e01eb9c6b
brucelin michael <michaelbrucelin@163.com>
2023-02-15 13:13:59 +0800

    20230215-131359

--- C#/LeetCode/QuestionBank/Question0455/Solution0455.cs
+++ C#/LeetCode/QuestionBank/Question0455/Solution0455.cs
@@ -8,9 +8,26 @@ namespace LeetCode.QuestionBank.Question0455
 {
     public class Solution0455 : Interface0455
     {
+        /// <summary>
+        /// 贪心
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public int FindContentChildren(int[] g, int[] s)
         {
-            throw new NotImplementedException();
+            Array.Sort(g); Array.Sort(s);
+            int result = 0, ptrg = 0, ptrs = 0;
+            while (ptrg < g.Length && ptrs < s.Length)
+            {
+                if (s[ptrs] >= g[ptrg])
+                {
+                    result++; ptrg++;
+                }
+                ptrs++;
+            }
+
+            return result;
         }
     }
 }
