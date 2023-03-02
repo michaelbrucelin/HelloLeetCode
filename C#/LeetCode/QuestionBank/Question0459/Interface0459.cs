54612332ba56f43549554c1e8b427d0886ef5045
brucelin michael <michaelbrucelin@163.com>
2023-03-02 17:53:07 +0800

    20230302-175306

---
+++ C#/LeetCode/QuestionBank/Question0459/Interface0459.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0459
+{
+    public interface Interface0459
+    {
+        public bool RepeatedSubstringPattern(string s);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question0461/Solution0461.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0461
+{
+    public class Solution0461 : Interface0461
+    {
+        public int HammingDistance(int x, int y)
+        {
+            return BitCount(x ^ y);
+        }
+
+        private int BitCount(int n)
+        {
+            int result = 0;
+
+            while (n > 0)
+            {
+                result++; n &= (n - 1);
+            }
+
+            return result;
+        }
+    }
+}
