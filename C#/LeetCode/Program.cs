2eaa7ce17447f14d17a60617e367bf7ed085c0a9
brucelin michael <michaelbrucelin@163.com>
2023-03-10 14:21:00 +0800

    20230310-142100

--- C#/LeetCode/Program.cs
+++ C#/LeetCode/Program.cs
@@ -1,5 +1,5 @@
 ﻿using LeetCode.Interview.Interview0502;
-using LeetCode.QuestionBank.Question2053;
+using LeetCode.QuestionBank.Question1590;
 using LeetCode.LCP.LCP0030;
 using LeetCode.剑指_Offer.剑指_Offer_0058_1;
 using LeetCode.剑指_Offer_II.剑指_Offer_II_0031;
@@ -23,7 +23,7 @@ namespace LeetCode
         {
             //Random random = new Random();
 
-            Test0058 test = new();
+            Test1590 test = new();
             test.Test();
             //test.TestDivergentTraverse();
             //test.Look4Rules();
@@ -102,6 +102,10 @@ namespace LeetCode
             {
                 //Console.WriteLine(int.Parse("03"));        // 3，不异常
                 //Console.WriteLine(Convert.ToInt32("03"));  // 3，不异常
+                //Console.WriteLine($"10 % 4 = {10 % 4}");
+                //Console.WriteLine($"10 % 4 = {Math.DivRem(10, 4).Quotient},{Math.DivRem(10, 4).Remainder}");
+                //Console.WriteLine($"10 %-4 = {10 % -4}");
+                //Console.WriteLine($"10 %-4 = {Math.DivRem(10, -4).Quotient},{Math.DivRem(10, -4).Remainder}");
             }
 
             {
---
+++ C#/LeetCode/QuestionBank/Question1590/Test1590.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1590
+{
+    public class Test1590
+    {
+        public void Test()
+        {
+            Interface1590 solution = new Solution1590();
+            int[] nums; int p;
+            int result, answer;
+            int id = 0;
+
+            // 1. 
+            nums = new int[] { 3, 1, 4, 2 }; p = 6; answer = 1;
+            result = solution.MinSubarray(nums, p);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 2. 
+            nums = new int[] { 6, 3, 5, 2 }; p = 9; answer = 2;
+            result = solution.MinSubarray(nums, p);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 3. 
+            nums = new int[] { 1, 2, 3 }; p = 3; answer = 0;
+            result = solution.MinSubarray(nums, p);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 4. 
+            nums = new int[] { 1, 2, 3 }; p = 7; answer = -1;
+            result = solution.MinSubarray(nums, p);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 5. 
+            nums = new int[] { 1000000000, 1000000000, 1000000000 }; p = 3; answer = 0;
+            result = solution.MinSubarray(nums, p);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+        }
+    }
+}
