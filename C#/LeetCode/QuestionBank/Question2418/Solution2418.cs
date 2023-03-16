678515605ff2f0fa6f3622ecd9196e06a476ff29
brucelin michael <michaelbrucelin@163.com>
2023-03-16 12:00:28 +0800

    20230316-120028

---
+++ C#/LeetCode/QuestionBank/Question2418/Solution2418.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2418
+{
+    public class Solution2418 : Interface2418
+    {
+        public string[] SortPeople2(string[] names, int[] heights)
+        {
+            int len = names.Length;
+            int[] ids = new int[len];
+            for (int i = 0; i < len; i++) ids[i] = i;
+            Array.Sort(ids, (id1, id2) => heights[id2] - heights[id1]);
+
+            string[] result = new string[len];
+            for (int i = 0; i < len; i++) result[i] = names[ids[i]];
+
+            return result;
+        }
+
+        /// <summary>
+        /// 与SortPeople()一样，原地更改
+        /// </summary>
+        /// <param name="names"></param>
+        /// <param name="heights"></param>
+        /// <returns></returns>
+        public string[] SortPeople(string[] names, int[] heights)
+        {
+            int len = names.Length;
+            int[] ids = new int[len];
+            for (int i = 0; i < len; i++) ids[i] = i;
+            Array.Sort(ids, (id1, id2) => heights[id2] - heights[id1]);
+
+            for (int i = 0; i < len; i++)
+            {
+                if (ids[i] != i)
+                {
+                    string _s = names[i]; names[i] = names[ids[i]]; names[ids[i]] = _s;
+                    int _i = ids[i]; ids[i] = i; ids[_i] = _i;
+                }
+            }
+
+            return names;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question2418/Test2418.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Utilses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2418
+{
+    public class Test2418
+    {
+        public void Test()
+        {
+            Interface2418 solution = new Solution2418();
+            string[] names; int[] heights;
+            string[] result, answer;
+            int id = 0;
+
+            // 1. 
+            names = new string[] { "Mary", "John", "Emma" }; heights = new int[] { 180, 165, 170 };
+            answer = new string[] { "Mary", "Emma", "John" };
+            result = solution.SortPeople(names, heights);
+            Console.WriteLine($"{++id,2}: {Utils.CompareArray(result, answer) + ",",-6} result: {Utils.ArrayToString(result)}, answer: {Utils.ArrayToString(answer)}");
+
+            // 2. 
+            names = new string[] { "Alice", "Bob", "Bob" }; heights = new int[] { 155, 185, 150 };
+            answer = new string[] { "Bob", "Alice", "Bob" };
+            result = solution.SortPeople(names, heights);
+            Console.WriteLine($"{++id,2}: {Utils.CompareArray(result, answer) + ",",-6} result: {Utils.ArrayToString(result)}, answer: {Utils.ArrayToString(answer)}");
+
+            // 3. 
+            names = new string[] { "GXLVEHVABFOGSFXUYYR", "TUHxnsxmu", "X", "OOYBLVKmzlaeaxbprc", "ARNLAPtfvmutkfsa", "XPMKPDUWOQEEILtbdjip", "QICEutjbr", "R" };
+            heights = new int[] { 11578, 89340, 73785, 12096, 55734, 89484, 59775, 72652 };
+            answer = new string[] { "XPMKPDUWOQEEILtbdjip", "TUHxnsxmu", "X", "R", "QICEutjbr", "ARNLAPtfvmutkfsa", "OOYBLVKmzlaeaxbprc", "GXLVEHVABFOGSFXUYYR" };
+            result = solution.SortPeople(names, heights);
+            Console.WriteLine($"{++id,2}: {Utils.CompareArray(result, answer) + ",",-6} result: {Utils.ArrayToString(result)}, answer: {Utils.ArrayToString(answer)}");
+        }
+    }
+}
