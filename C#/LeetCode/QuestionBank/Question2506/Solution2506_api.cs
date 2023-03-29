b98e7c96f787dfb1f913db9729e9b5545fb4f748
brucelin michael <michaelbrucelin@163.com>
2023-03-29 11:59:51 +0800

    20230329-115951

---
+++ C#/LeetCode/QuestionBank/Question2506/Solution2506_api.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2506
+{
+    public class Solution2506_api : Interface2506
+    {
+        public int SimilarPairs(string[] words)
+        {
+            return words.Select(s => s.Select(c => 1 << (c - 'a')).Aggregate((i1, i2) => i1 | i2))
+                        .GroupBy(i => i)
+                        .Select(g => g.Count())
+                        .Sum(i => i * (i - 1) >> 1);
+        }
+    }
+}
