64fd35460bdee33a51f3ffd9a35e3cbbd4b54621
brucelin michael <michaelbrucelin@163.com>
2023-03-30 11:22:11 +0800

    20230330-112210

---
+++ C#/LeetCode/QuestionBank/Question0859/Interface0859.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0859
+{
+    public interface Interface0859
+    {
+        public bool BuddyStrings(string s, string goal);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1266/Solution1266_pai.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1266
+{
+    public class Solution1266_pai : Interface1266
+    {
+        public int MinTimeToVisitAllPoints(int[][] points)
+        {
+            return points.Skip(1)
+                         .Zip(points, (arr1, arr2) => Math.Max(Math.Abs(arr1[0] - arr2[0]), Math.Abs(arr1[1] - arr2[1])))
+                         .Sum();
+        }
+    }
+}
