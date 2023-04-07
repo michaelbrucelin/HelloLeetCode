983679f168498c3c2dd6830d4e1dc4d4d5361a4c
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-04-07 16:55:34 +0800

    20230407-165530

---
+++ C#/LeetCode/QuestionBank/Question1040/Interface1040.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1040
+{
+    public interface Interface1040
+    {
+        public int[] NumMovesStonesII(int[] stones);
+    }
+}
