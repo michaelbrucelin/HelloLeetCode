48e385eca0ad9a906c344b8a5c270580e902e460
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-04-11 12:01:53 +0800

    20230411-120152

---
+++ C#/LeetCode/QuestionBank/Question1041/Interface1041.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1041
+{
+    public interface Interface1041
+    {
+        public bool IsRobotBounded(string instructions);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1041/Solution1041.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1041
+{
+    public class Solution1041 : Interface1041
+    {
+        /// <summary>
+        /// 分析
+        /// 具体分析见Solution1041.md
+        /// 
+        /// 1. 先一轮移动，根据移动后状态（位置与方向）判断
+        /// </summary>
+        /// <param name="instructions"></param>
+        /// <returns></returns>
+        public bool IsRobotBounded(string instructions)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 与IsRobotBounded()一样，直接移动4轮，验证移动后是否回到了原点
+        /// </summary>
+        /// <param name="instructions"></param>
+        /// <returns></returns>
+        public bool IsRobotBounded2(string instructions)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
