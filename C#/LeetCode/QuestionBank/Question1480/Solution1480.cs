8481e6b7fc43f2bc5d12c58e73c36004b095aac1
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-04-18 14:32:22 +0800

    20230418-143221

---
+++ C#/LeetCode/QuestionBank/Question1480/Solution1480.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1480
+{
+    public class Solution1480 : Interface1480
+    {
+        /// <summary>
+        /// 非原地
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] RunningSum(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            result[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++) result[i] = result[i - 1] + nums[i];
+
+            return result;
+        }
+
+        /// <summary>
+        /// 原地
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] RunningSum2(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++) nums[i] += nums[i - 1];
+
+            return nums;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1688/Solution1688.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1688
+{
+    public class Solution1688 : Interface1688
+    {
+        public int NumberOfMatches(int n)
+        {
+            int result = 0;
+            while (n > 1)
+            {
+                result += n >> 1;
+                if ((n & 1) != 1) n >>= 1; else n = (n >> 1) + 1;
+            }
+
+            return result;
+        }
+    }
+}
