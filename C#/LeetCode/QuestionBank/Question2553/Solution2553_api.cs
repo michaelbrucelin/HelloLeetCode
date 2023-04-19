b4a7aab4a5d3fa077431d5c9988da0fe9df313a1
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-04-19 15:20:34 +0800

    20230419-152034

---
+++ C#/LeetCode/QuestionBank/Question2553/Solution2553_api.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2553
+{
+    public class Solution2553_api : Interface2553
+    {
+        public int[] SeparateDigits(int[] nums)
+        {
+            return string.Join("", nums.Select(i => i.ToString()))
+                         .ToCharArray()
+                         .Select(c => c & 15)
+                         .ToArray();
+        }
+
+        public int[] SeparateDigits2(int[] nums)
+        {
+            return nums.Select(i => i.ToString().ToCharArray())
+                       .SelectMany(c => c)
+                       .Select(c => c & 15)
+                       .ToArray();
+        }
+    }
+}
