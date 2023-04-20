0350957e3c1f3fc306bd4199054b07d1764f1c05
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-04-21 07:43:25 +0800

    20230421-074324

---
+++ C#/LeetCode/QuestionBank/Question2413/Interface2413.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2413
+{
+    public interface Interface2413
+    {
+        public int SmallestEvenMultiple(int n);
+    }
+}
