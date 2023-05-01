3e04ce8f070e8a01957e7182b3f2d3852227ed9a
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-05-01 08:43:49 +0800

    2023-05-01

---
+++ C#/LeetCode/QuestionBank/Question1376/Interface1376.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1376
+{
+    public interface Interface1376
+    {
+        public int NumOfMinutes(int n, int headID, int[] manager, int[] informTime);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1376/Solution1376.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1376
+{
+    public class Solution1376 : Interface1376
+    {
+        public int NumOfMinutes(int n, int headID, int[] manager, int[] informTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
