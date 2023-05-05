05462f7eb62d56003c02cad5c2bf33169e2ce44e
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-05-05 08:27:45 +0800

    20230505-082744

--- C#/LeetCode/QuestionBank/Question2432/Solution2432.cs
+++ C#/LeetCode/QuestionBank/Question2432/Solution2432.cs
@@ -28,25 +28,5 @@ namespace LeetCode.QuestionBank.Question2432
 
             return result;
         }
-
-        public int HardestWorker2(int n, int[][] logs)
-        {
-            if (logs.Length == 1) return logs[0][0];
-
-            return logs.Select((i, id) => id == 0 ? (i[0], i[1]) : (i[0], i[1] - logs[id - 1][1]))
-                       .OrderByDescending(item => item.Item2)
-                       .ThenBy(item => item.Item1)
-                       .First().Item1;
-        }
-
-        public int HardestWorker3(int n, int[][] logs)
-        {
-            if (logs.Length == 1) return logs[0][0];
-
-            var comparer = Comparer<(int, int)>.Create((item1, item2) => item1.Item2 != item2.Item2 ? item1.Item2 - item2.Item2 : item2.Item1 - item1.Item1);
-
-            return logs.Select((i, id) => id == 0 ? (i[0], i[1]) : (i[0], i[1] - logs[id - 1][1]))
-                       .Max(comparer).Item1;
-        }
     }
 }
