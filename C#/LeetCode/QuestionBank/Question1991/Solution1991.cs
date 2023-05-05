89c9d9f1c6a49ed93171bba512ba70ad4cd80dc5
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-05-05 10:05:15 +0800

    20230505-100515

--- C#/LeetCode/QuestionBank/Question1991/Solution1991.cs
+++ C#/LeetCode/QuestionBank/Question1991/Solution1991.cs
@@ -8,9 +8,17 @@ namespace LeetCode.QuestionBank.Question1991
 {
     public class Solution1991 : Interface1991
     {
+        /// <summary>
+        /// 暴力解
+        /// 题目的数量级可以暴力解的，写着玩玩，试试切片
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
         public int FindMiddleIndex(int[] nums)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[0..i].Sum() == nums[(i + 1)..].Sum()) return i;
+            return -1;
         }
     }
 }
---
+++ C#/LeetCode/QuestionBank/Question1991/Solution1991_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1991
+{
+    public class Solution1991_2 : Interface1991
+    {
+        /// <summary>
+        /// 前缀和 + 后缀和
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int FindMiddleIndex(int[] nums)
+        {
+            if (nums.Length == 1) return 0;
+
+            int prev = 0, tail = nums[1..].Sum();
+            if (tail == 0) return 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                prev += nums[i - 1]; tail -= nums[i];
+                if (prev == tail) return i;
+            }
+
+            return -1;
+        }
+    }
+}
