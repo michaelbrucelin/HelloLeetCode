155d39bda9a5b7d27aa51f1a90abbcb45a9de59f
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-05-05 12:00:45 +0800

    20230505-120045

--- C#/LeetCode/QuestionBank/Question2144/Solution2144.cs
+++ C#/LeetCode/QuestionBank/Question2144/Solution2144.cs
@@ -8,9 +8,39 @@ namespace LeetCode.QuestionBank.Question2144
 {
     public class Solution2144 : Interface2144
     {
+        /// <summary>
+        /// 贪心
+        /// </summary>
+        /// <param name="cost"></param>
+        /// <returns></returns>
         public int MinimumCost(int[] cost)
         {
-            throw new NotImplementedException();
+            Array.Sort(cost, (i1, i2) => i2 - i1);
+
+            int result = 0;
+            for (int i = 0; i < cost.Length; i++)
+                if (i % 3 != 2) result += cost[i];
+
+            return result;
+        }
+
+        /// <summary>
+        /// 贪心 + 状态机
+        /// </summary>
+        /// <param name="cost"></param>
+        /// <returns></returns>
+        public int MinimumCost2(int[] cost)
+        {
+            Array.Sort(cost, (i1, i2) => i2 - i1);
+
+            int result = 0, state = 2;
+            for (int i = 0; i < cost.Length; i++)
+            {
+                state = state switch { 2 => 0, 0 => 1, 1 => 2 };
+                if (state != 2) result += cost[i];
+            }
+
+            return result;
         }
     }
 }
---
+++ C#/LeetCode/QuestionBank/Question2144/Solution2144_api.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2144
+{
+    public class Solution2144_api : Interface2144
+    {
+        public int MinimumCost(int[] cost)
+        {
+            return cost.OrderBy(i => -i)
+                       .Where((i, id) => id % 3 != 2)
+                       .Sum();
+        }
+    }
+}
