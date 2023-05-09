c926c95502cc6b34b1798cee26ab98527cedb197
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-05-09 11:51:35 +0800

    20230509-115135

---
+++ C#/LeetCode/QuestionBank/Question1263/Interface1263.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1263
+{
+    public interface Interface1263
+    {
+        public int MinPushBox(char[][] grid);
+    }
+}
