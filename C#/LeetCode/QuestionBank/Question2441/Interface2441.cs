8656e5ae656986734b27f60c792fa375a9982547
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-05-13 10:27:05 +0800

    20230513-102703

---
+++ C#/LeetCode/QuestionBank/Question2441/Interface2441.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2441
+{
+    public interface Interface2441
+    {
+        public int FindMaxK(int[] nums);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question2441/Solution2441.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2441
+{
+    public class Solution2441 : Interface2441
+    {
+        public int FindMaxK(int[] nums)
+        {
+            int result = -1;
+            HashSet<int> set = new HashSet<int>(nums);
+            foreach (int num in set)
+                if (set.Contains(-num)) result = Math.Max(result, Math.Abs(num));
+
+            return result;
+        }
+    }
+}
