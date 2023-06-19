a74d91072aa25b2cd678d7d24f3cc464c70312ff
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-06-19 12:02:18 +0800

    20230619-120218

---
+++ C#/LeetCode/QuestionBank/Question1254/Solution1254_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1254
+{
+    public class Solution1254_2 : Interface1254
+    {
+        public int ClosedIsland(int[][] grid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1254/Test1254.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1254
+{
+    public class Test1254
+    {
+        public void Test()
+        {
+            Interface1254 solution = new Solution1254();
+            int[][] grid;
+            int result, answer;
+            int id = 0;
+
+            // 1. 
+
+            // 2. 
+
+            // 3. 
+        }
+    }
+}
