09e944ea810c0ecce6ccc1dfbccb076f7b044d0f
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-06-24 14:41:27 +0800

    20230624-144127

---
+++ C#/LeetCode/QuestionBank/Question1659/Interface1659.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1659
+{
+    public interface Interface1659
+    {
+        public int GetMaxGridHappiness(int m, int n, int introvertsCount, int extrovertsCount);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1659/Solution1659.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1659
+{
+    public class Solution1659 : Interface1659
+    {
+        public int GetMaxGridHappiness(int m, int n, int introvertsCount, int extrovertsCount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
