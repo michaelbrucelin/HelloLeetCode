57b7988777d9721cf5f2dcad2582f6e073f6a48e
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-06-29 13:02:58 +0800

    20230629-130257

---
+++ C#/LeetCode/QuestionBank/Question1030/Interface1030.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1030
+{
+    public interface Interface1030
+    {
+        public int[][] AllCellsDistOrder(int rows, int cols, int rCenter, int cCenter);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1941/Interface1941.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1941
+{
+    public interface Interface1941
+    {
+        public bool AreOccurrencesEqual(string s);
+    }
+}
