e2f0ff3f10f5eb61eb1f3bd6cef97c9607468df5
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-07-10 11:57:44 +0800

    20230710-115744

---
+++ C#/LeetCode/QuestionBank/Question2176/Solution2176.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2176
+{
+    public class Solution2176 : Interface2176
+    {
+        public int CountPairs(int[] nums, int k)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
