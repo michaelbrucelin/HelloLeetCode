242c87f59931526d0bf25c6634c126ee2b1d4317
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-07-28 08:54:49 +0800

    20230728-085449

---
+++ C#/LeetCode/QuestionBank/Question2050/Interface2050.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2050
+{
+    public interface Interface2050
+    {
+        public int MinimumTime(int n, int[][] relations, int[] time);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question2050/Solution2050.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2050
+{
+    public class Solution2050 : Interface2050
+    {
+        public int MinimumTime(int n, int[][] relations, int[] time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
