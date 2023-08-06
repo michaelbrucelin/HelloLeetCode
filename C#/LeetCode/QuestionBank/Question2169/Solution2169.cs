e7bf7cf1180887b6c5b8449ca648ae8283717ef4
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-08-06 12:25:17 +0800

    20230806-122517

---
+++ C#/LeetCode/QuestionBank/Question2169/Solution2169.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2169
+{
+    public class Solution2169 : Interface2169
+    {
+        /// <summary>
+        /// 模拟
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public int CountOperations(int num1, int num2)
+        {
+            int result = 0;
+            while (num1 > 0 && num2 > 0)
+            {
+                result++;
+                if (num1 >= num2) num1 -= num2; else num2 -= num1;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// “倍速”模拟
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public int CountOperations2(int num1, int num2)
+        {
+            int result = 0;
+            while (num1 > 0 && num2 > 0)
+            {
+                if (num1 >= num2)
+                {
+                    result += num1 / num2; num1 %= num2;
+                }
+                else
+                {
+                    result += num2 / num1; num2 %= num1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question2744/Solution2744.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2744
+{
+    public class Solution2744 : Interface2744
+    {
+        /// <summary>
+        /// 哈希表
+        /// </summary>
+        /// <param name="words"></param>
+        /// <returns></returns>
+        public int MaximumNumberOfStringPairs(string[] words)
+        {
+            HashSet<string> set = new HashSet<string>(words);
+
+            int result = 0;
+            foreach (string word in set)
+            {
+                if (word[0] != word[1] && set.Contains($"{word[1]}{word[0]}")) result++;
+            }
+
+            return result >> 1;
+        }
+
+        /// <summary>
+        /// 哈希表2
+        /// </summary>
+        /// <param name="words"></param>
+        /// <returns></returns>
+        public int MaximumNumberOfStringPairs2(string[] words)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            int result = 0;
+            foreach (string word in words)
+            {
+                if (set.Contains($"{word[1]}{word[0]}")) result++; else set.Add(word);
+            }
+
+            return result;
+        }
+    }
+}
