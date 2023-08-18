ca607bc0621b05895873958a4245db83e81c2f14
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-08-18 14:05:58 +0800

    20230818-140557

---
+++ C#/LeetCode/QuestionBank/Question1388/Solution1388.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1388
+{
+    public class Solution1388 : Interface1388
+    {
+        /// <summary>
+        /// DFS + 记忆化搜索
+        /// 将数组中的值使用 - 分割拼接为字符串作为记忆化的状态（key）
+        /// 
+        /// 逻辑没问题，提交会TLE，参考测试用例03
+        /// </summary>
+        /// <param name="slices"></param>
+        /// <returns></returns>
+        public int MaxSizeSlices(int[] slices)
+        {
+            Dictionary<string, int> memory = new Dictionary<string, int>();
+            return dfs(slices, memory);
+        }
+
+        private int dfs(int[] slices, Dictionary<string, int> memory)
+        {
+            string key = GetKey(slices);
+            if (memory.ContainsKey(key)) return memory[key];
+            if (slices.Length == 0) { memory.Add("", 0); return 0; }
+
+            int sum = 0;
+            for (int i = 0; i < slices.Length; i++)
+                sum = Math.Max(sum, slices[i] + dfs(CutSlices(slices, i), memory));
+            memory.Add(key, sum);
+
+            return memory[key];
+        }
+
+        private string GetKey(int[] slices)
+        {
+            if (slices.Length == 0) return "";
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < slices.Length; i++)
+            {
+                sb.Append(slices[i]); sb.Append('-');
+            }
+
+            return sb.ToString();
+        }
+
+        private int[] CutSlices(int[] slices, int id)
+        {
+            int len = slices.Length;
+            int[] result = new int[len - 3];
+            if (len == 3) return result;
+
+            if (id == 0)
+                Array.Copy(slices, 2, result, 0, len - 3);
+            else if (id == 1)
+                Array.Copy(slices, 3, result, 0, len - 3);
+            else if (id == len - 1)
+                Array.Copy(slices, 1, result, 0, len - 3);
+            else if (id == len - 2)
+                Array.Copy(slices, 0, result, 0, len - 3);
+            else
+            {
+                for (int i = 0; i < id - 1; i++) result[i] = slices[i];
+                for (int i = id + 2; i < len; i++) result[i - 3] = slices[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1388/Test1388.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1388
+{
+    public class Test1388
+    {
+        public void Test()
+        {
+            Interface1388 solution = new Solution1388();
+            int[] slices;
+            int result, answer;
+            int id = 0;
+
+            // 1. 
+            slices = new int[] { 1, 2, 3, 4, 5, 6 };
+            answer = 10;
+            result = solution.MaxSizeSlices(slices);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 2. 
+            slices = new int[] { 8, 9, 8, 6, 1, 1 };
+            answer = 16;
+            result = solution.MaxSizeSlices(slices);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 3. 
+            slices = new int[] { 3, 9, 4, 5, 3, 8, 1, 10, 3, 7, 2, 9, 10, 2, 6, 2, 9, 8, 7, 10, 7, 5, 1, 6, 5, 8, 9, 10, 6, 5, 7, 7, 2, 5, 3, 10, 4, 3, 4 };
+            answer = 110;
+            result = solution.MaxSizeSlices(slices);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+        }
+    }
+}
