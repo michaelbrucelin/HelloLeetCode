1afa757c2001f1d3c9af6fe68a3c7e9e45bc9dc6
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-09-10 16:49:43 +0800

    20230910-164943

---
+++ C#/LeetCode/QuestionBank/Question0207/Solution0207_2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0207
+{
+    public class Solution0207_2 : Interface0207
+    {
+        /// <summary>
+        /// 拓扑排序，BFS
+        /// </summary>
+        /// <param name="numCourses"></param>
+        /// <param name="prerequisites"></param>
+        /// <returns></returns>
+        public bool CanFinish(int numCourses, int[][] prerequisites)
+        {
+            (int _in, List<int> adj)[] graph = new (int _in, List<int> adj)[numCourses];
+            for (int i = 0; i < numCourses; i++) graph[i] = (0, new List<int>());
+            foreach (var arr in prerequisites)
+            {
+                graph[arr[0]]._in++; graph[arr[1]].adj.Add(arr[0]);
+            }
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < numCourses; i++) if (graph[i]._in == 0) queue.Enqueue(i);
+
+            List<int> result = new List<int>();
+            int vid; while (queue.Count > 0)
+            {
+                vid = queue.Dequeue();
+                result.Add(vid);
+                foreach (int _vid in graph[vid].adj)
+                {
+                    if (--graph[_vid]._in == 0) queue.Enqueue(_vid);
+                }
+            }
+
+            return result.Count == numCourses;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question0210/Solution0210_2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0210
+{
+    public class Solution0210_2 : Interface0210
+    {
+        /// <summary>
+        /// 拓扑排序，DFS
+        /// 
+        /// 错误的，错误发生在判断环上面，例如:[[1,0],[2,0],[3,1],[3,2]]
+        /// 0 --> 1 --> 3
+        /// |          /\
+        /// | --> 2 --- |
+        /// </summary>
+        /// <param name="numCourses"></param>
+        /// <param name="prerequisites"></param>
+        /// <returns></returns>
+        public int[] FindOrder(int numCourses, int[][] prerequisites)
+        {
+            List<int>[] graph = new List<int>[numCourses];
+            for (int i = 0; i < numCourses; i++) graph[i] = new List<int>();
+            foreach (var arr in prerequisites) graph[arr[1]].Add(arr[0]);
+            int[] visited = new int[numCourses];
+
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < numCourses; i++)
+            {
+                if (visited[i] != 0) continue;
+                if (dfs(graph, visited, i, stack)) return new int[] { };
+            }
+
+            int[] result = new int[numCourses];
+            for (int i = 0; i < numCourses; i++) result[i] = stack.Pop();
+            return result;
+        }
+
+        private bool dfs(List<int>[] graph, int[] visited, int vid, Stack<int> stack)
+        {
+            if (visited[vid] == 1) return true; visited[vid] = 1;
+
+            foreach (int _vid in graph[vid])
+                if (dfs(graph, visited, _vid, stack)) return true;
+            stack.Push(vid);
+
+            return false;
+        }
+    }
+}
