c5bdbaa2284c038169edc82efc5d48572001e1fb
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-09-11 10:12:49 +0800

    20230911-101248

---
+++ C#/LeetCode/QuestionBank/Question0630/Interface0630.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0630
+{
+    public interface Interface0630
+    {
+        public int ScheduleCourse(int[][] courses);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question0630/Solution0630.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0630
+{
+    public class Solution0630 : Interface0630
+    {
+        public int ScheduleCourse(int[][] courses)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
