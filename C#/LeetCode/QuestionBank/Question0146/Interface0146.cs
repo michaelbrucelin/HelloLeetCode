1e4db60e7f094edc8c3c65d95780022b046420bf
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-09-24 10:44:32 +0800

    20230924-104431

---
+++ C#/LeetCode/QuestionBank/Question0146/Interface0146.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0146
+{
+    /// <summary>
+    /// Your LRUCache object will be instantiated and called as such:
+    /// LRUCache obj = new LRUCache(capacity);
+    /// int param_1 = obj.Get(key);
+    /// obj.Put(key,value);
+    /// </summary>
+    public interface Interface0146
+    {
+        public int Get(int key);
+
+        public void Put(int key, int value);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question0146/Solution0146.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0146
+{
+    public class Solution0146
+    {
+    }
+
+    public class LRUCache : Interface0146
+    {
+        public LRUCache(int capacity)
+        {
+            this.capacity = capacity;
+            cache = new Dictionary<int, int>();
+            order = new Dictionary<int, int>();
+        }
+
+        private Dictionary<int, int> cache;
+        private Dictionary<int, int> order;
+        private int capacity;
+
+        public int Get(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Put(int key, int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
