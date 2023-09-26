3c7413aa95c28c94ecd3e8b1b62b492dfecf3e4d
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-09-26 09:45:09 +0800

    20230926-094508

---
+++ C#/LeetCode/QuestionBank/Question2582/Solution2582.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2582
+{
+    public class Solution2582 : Interface2582
+    {
+        /// <summary>
+        /// 模拟
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public int PassThePillow(int n, int time)
+        {
+            int ptr = 1, dir = 1;
+            while (--time >= 0)
+            {
+                ptr += dir;
+                if (ptr == n) dir = -1; else if (ptr == 1) dir = 1;
+            }
+
+            return ptr;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question2582/Solution2582_2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2582
+{
+    public class Solution2582_2 : Interface2582
+    {
+        /// <summary>
+        /// 数学
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public int PassThePillow(int n, int time)
+        {
+            var t = Math.DivRem(time, n - 1);
+            return (t.Quotient & 1) == 0 ? t.Remainder + 1 : n - t.Remainder;
+        }
+    }
+}
