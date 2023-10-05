29896e41d3b878aaebcfefea0936a54c6bb7d5aa
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-10-05 16:07:59 +0800

    20231005-160752

---
+++ C#/LeetCode/QuestionBank/Question0309/Interface0309.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0309
+{
+    public interface Interface0309
+    {
+        public int MaxProfit(int[] prices);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question0309/Solution0309.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0309
+{
+    public class Solution0309 : Interface0309
+    {
+        public int MaxProfit(int[] prices)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
