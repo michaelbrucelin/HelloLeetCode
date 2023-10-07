07f09e306f7fcbfa76b5cd5b8a8c08726b1818cb
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-10-07 15:22:21 +0800

    20231007-152221

---
+++ C#/LeetCode/QuestionBank/Question2136/Interface2136.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2136
+{
+    public interface Interface2136
+    {
+        public int EarliestFullBloom(int[] plantTime, int[] growTime);
+    }
+}
