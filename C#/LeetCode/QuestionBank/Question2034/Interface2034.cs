625de4b7ca8f22c2f5b66ee00ead7a12202f3bc4
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-10-08 12:31:03 +0800

    20231008-123102

---
+++ C#/LeetCode/QuestionBank/Question2034/Interface2034.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2034
+{
+    /// <summary>
+    /// Your StockPrice object will be instantiated and called as such:
+    /// StockPrice obj = new StockPrice();
+    /// obj.Update(timestamp,price);
+    /// int param_2 = obj.Current();
+    /// int param_3 = obj.Maximum();
+    /// int param_4 = obj.Minimum();
+
+    /// </summary>
+    public interface Interface2034
+    {
+        public void Update(int timestamp, int price);
+
+        public int Current();
+
+        public int Maximum();
+
+        public int Minimum();
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question2034/Solution2034.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2034
+{
+    public class Solution2034
+    {
+    }
+
+    /// <summary>
+    /// 双字典
+    /// Dictionary<int, int>                 key 时间戳 value 价格
+    /// SortedDictionary<int, HashSet<int>>  key 价格   value 时间戳
+    /// 
+    /// 超出时间限制了，没想明白为什么会TLE
+    /// </summary>
+    public class StockPrice : Interface2034
+    {
+        public StockPrice()
+        {
+            t2p = new Dictionary<int, int>();
+            p2t = new SortedDictionary<int, HashSet<int>>();
+            cur_time = -1;
+        }
+
+        private Dictionary<int, int> t2p;
+        private SortedDictionary<int, HashSet<int>> p2t;
+        private int cur_time;
+
+        public int Current()
+        {
+            return t2p[cur_time];
+        }
+
+        public int Maximum()
+        {
+            return p2t.Last().Key;   // 题目确保了p2t不为空
+        }
+
+        public int Minimum()
+        {
+            return p2t.First().Key;  // 题目确保了p2t不为空
+        }
+
+        public void Update(int timestamp, int price)
+        {
+            if (timestamp >= cur_time) cur_time = timestamp;
+
+            if (t2p.ContainsKey(timestamp))  // update
+            {
+                int _price = t2p[timestamp];
+                t2p[timestamp] = price;
+                if (p2t.ContainsKey(price)) p2t[price].Add(timestamp); else p2t.Add(price, new HashSet<int>() { timestamp });
+                if (p2t[_price].Count != 1) p2t[_price].Remove(timestamp); else p2t.Remove(_price);
+            }
+            else                             // insert
+            {
+                t2p.Add(timestamp, price);
+                if (p2t.ContainsKey(price)) p2t[price].Add(timestamp); else p2t.Add(price, new HashSet<int>() { timestamp });
+            }
+        }
+    }
+}
