29119112279a22839feb58a2dd013d081cd3d888
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-10-23 09:13:04 +0800

    20231023-091304

---
+++ C#/LeetCode/QuestionBank/Question1402/Solution1402.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1402
+{
+    public class Solution1402 : Interface1402
+    {
+        public int MaxSatisfaction(int[] satisfaction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
