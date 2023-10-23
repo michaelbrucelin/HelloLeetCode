3edc6473bbd19bbb1810dbb5e53954b9929c1c0d
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-10-23 09:00:09 +0800

    20231023-090008

---
+++ C#/LeetCode/QuestionBank/Question2678/Solution2678.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2678
+{
+    public class Solution2678 : Interface2678
+    {
+        public int CountSeniors(string[] details)
+        {
+            int result = 0;
+            foreach (string str in details)
+                if ((str[11] - '0') * 10 + (str[12] - '0') > 60) result++;
+
+            return result;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question2678/Solution2678_api.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2678
+{
+    public class Solution2678_api : Interface2678
+    {
+        public int CountSeniors(string[] details)
+        {
+            return details.Count(s => int.Parse(s[11..13]) > 60);
+        }
+    }
+}
