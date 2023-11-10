759e75440868ddca0d0dfb6fe4f1f8a204c605bf
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-11-10 11:41:08 +0800

    20231110-114107

---
+++ C#/LeetCode/QuestionBank/Question1290/Solution1290.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1290
+{
+    public class Solution1290 : Interface1290
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            int result = 0;
+            ListNode ptr = head;
+            while (ptr != null)
+            {
+                result = (result << 1) | ptr.val; ptr = ptr.next;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1323/Solution1323.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1323
+{
+    public class Solution1323 : Interface1323
+    {
+        public int Maximum69Number(int num)
+        {
+            Stack<int> stack = new Stack<int>();
+            while (num > 0) { stack.Push(num % 10); num /= 10; }
+
+            int result = 0, digit; bool flag = true;
+            while (stack.Count > 0)
+            {
+                digit = stack.Pop();
+                if (flag && digit == 6)
+                {
+                    result = result * 10 + 9; flag = false;
+                }
+                else
+                {
+                    result = result * 10 + digit;
+                }
+            }
+
+            return result;
+        }
+    }
+}
