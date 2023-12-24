b5328092e012f46cd59b1716516a8ec826929393
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-12-24 12:09:40 +0800

    20231224-120938

---
+++ C#/LeetCode/QuestionBank/Question1954/Solution1954.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1954
+{
+    public class Solution1954 : Interface1954
+    {
+        /// <summary>
+        /// 二分法
+        /// 如果边长为2x，一个象限的种树数量为：x(x+1)^2，四个象限总种树数量为：4x(x+1)^2-2x(x+1) = 2x(x+1)(2x+1)
+        /// </summary>
+        /// <param name="neededApples"></param>
+        /// <returns></returns>
+        public long MinimumPerimeter(long neededApples)
+        {
+            long result = -1, _result, low = 1, high = 100000, mid;  // 根据上面的推导，high = 100000足够了
+            while (low <= high)
+            {
+                mid = low + ((high - low) >> 1);
+                _result = (mid * (mid + 1) << 1) * ((mid << 1) + 1);
+                if (_result >= neededApples)
+                {
+                    result = mid << 3; high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1954/Test1954.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1954
+{
+    public class Test1954
+    {
+        public void Test()
+        {
+            Interface1954 solution = new Solution1954();
+            long neededApples;
+            long result, answer;
+            int id = 0;
+
+            // 1. 
+            neededApples = 1;
+            answer = 8;
+            result = solution.MinimumPerimeter(neededApples);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 2. 
+            neededApples = 13;
+            answer = 16;
+            result = solution.MinimumPerimeter(neededApples);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 3. 
+            neededApples = 1000000000;
+            answer = 5040;
+            result = solution.MinimumPerimeter(neededApples);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 4. 
+            neededApples = 1000000000000000;
+            answer = 503968;
+            result = solution.MinimumPerimeter(neededApples);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+        }
+    }
+}
