2208c6c9f9f7406f298b5d7fb43c73db98bb8866
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-01-19 09:03:20 +0800

    20240119-090319

---
+++ C#/LeetCode/QuestionBank/Question2809/Interface2809.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2809
+{
+    public interface Interface2809
+    {
+        public int MinimumTime(IList<int> nums1, IList<int> nums2, int x);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question2809/Solution2809.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2809
+{
+    public class Solution2809 : Interface2809
+    {
+        public int MinimumTime(IList<int> nums1, IList<int> nums2, int x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
