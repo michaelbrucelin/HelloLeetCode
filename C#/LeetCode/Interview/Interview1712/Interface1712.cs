547d83f8b5afe58562bacc641418cdf157f46466
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-03-13 13:12:52 +0800

    20240313-131251

---
+++ C#/LeetCode/Interview/Interview1712/Interface1712.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Interview.Interview1712
+{
+    public interface Interface1712
+    {
+        public TreeNode ConvertBiNode(TreeNode root);
+    }
+}
---
+++ C#/LeetCode/Interview/Interview1712/Solution1712.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Interview.Interview1712
+{
+    public class Solution1712 : Interface1712
+    {
+        /// <summary>
+        /// DFS
+        /// 中序遍历
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public TreeNode ConvertBiNode(TreeNode root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
