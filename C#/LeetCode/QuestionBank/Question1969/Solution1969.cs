36f25cb5ad9320d83679ebdc35fcfd46bf7f3c24
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-03-20 15:56:31 +0800

    20240320-155630

---
+++ C#/LeetCode/QuestionBank/Question1969/Solution1969.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1969
+{
+    public class Solution1969 : Interface1969
+    {
+        /// <summary>
+        /// 数学构造
+        /// 1. [1, 2^p-1] 中所有数字放一起统计，各个二进制位上 1 与 0 的数量是固定的，每一位上都有 2^(p-1) 个 1
+        /// 2. 无论怎么交换，所有数字的和是不变的
+        ///     所以就相当于构造 2^p-1 个正整数，每个位上有 2^(p-1) 个 1，并使其乘积最小即可
+        /// 3. 如果两个正整数的和固定，这两个正整数越接近，乘积越大，越离散，乘积越小
+        ///     所以，只要分为 2^(p-1)-1 个 1, 2^(p-1)-1 个 2^p-2, 1 个 2^p-1 乘积就是最小的，具体证明见Solution1969.md
+        ///     2^(p-1)-1 个 2^p-2 可以使用快速幂计算
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public int MinNonZeroProduct(int p)
+        {
+            const int MOD = (int)1e9 + 7;
+            long _p = (1L << p) - 1;
+            long result = _p % MOD, _result = --_p % MOD;
+            for (int i = 1; i < p; i++)
+            {
+                result = result * _result % MOD;
+                _result = _result * _result % MOD;
+            }
+
+            return (int)result;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1969/Test1969.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1969
+{
+    public class Test1969
+    {
+        public void Test()
+        {
+            Interface1969 solution = new Solution1969();
+            int p;
+            int result, answer;
+            int id = 0;
+
+            // 1. 
+            p = 1;
+            answer = 1;
+            result = solution.MinNonZeroProduct(p);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 2. 
+            p = 2;
+            answer = 6;
+            result = solution.MinNonZeroProduct(p);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 3. 
+            p = 3;
+            answer = 1512;
+            result = solution.MinNonZeroProduct(p);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 4. 
+            p = 60;
+            answer = 813987236;
+            result = solution.MinNonZeroProduct(p);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+        }
+    }
+}
