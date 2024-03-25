e883bc4679d2fdfe29515840ad0dd8117affd327
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-03-25 11:51:46 +0800

    20240325-115146

--- C#/LeetCode/QuestionBank/Question0518/Solution0518_off.cs
+++ C#/LeetCode/QuestionBank/Question0518/Solution0518_off.cs
@@ -8,6 +8,13 @@ namespace LeetCode.QuestionBank.Question0518
 {
     public class Solution0518_off : Interface0518
     {
+        /// <summary>
+        /// DP
+        /// 有序DP，所以不需要记录每一种组合的状态
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="coins"></param>
+        /// <returns></returns>
         public int Change(int amount, int[] coins)
         {
             int[] dp = new int[amount + 1];
