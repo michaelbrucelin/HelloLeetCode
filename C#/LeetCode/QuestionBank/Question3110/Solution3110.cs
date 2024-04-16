1683b16eb8fa2a191460d5d8876d829cebd2dace
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-04-16 14:04:55 +0800

    20240416-140451

--- C#/LeetCode/QuestionBank/Question3110/Solution3110.cs
+++ C#/LeetCode/QuestionBank/Question3110/Solution3110.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace LeetCode.QuestionBank.Question3110
 {
-    internal class Solution3110
+    public class Solution3110 : Interface3110
     {
+        /// <summary>
+        /// 模拟
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int ScoreOfString(string s)
+        {
+            int result = 0;
+            for (int i = 1; i < s.Length; i++) result += Math.Abs(s[i] - s[i - 1]);
+
+            return result;
+        }
     }
 }
