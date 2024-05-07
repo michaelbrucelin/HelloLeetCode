b04ea388040d9899b1f77ff03f7fceae07ff75c7
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-05-07 11:20:07 +0800

    20240507-112005

---
+++ C#/LeetCode/QuestionBank/Question1463/Interface1463.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1463
+{
+    public interface Interface1463
+    {
+        public int CherryPickup(int[][] grid);
+    }
+}
--- C#/LeetCode/QuestionBank/Question1463/Solution1463.cs
+++ C#/LeetCode/QuestionBank/Question1463/Solution1463.cs
@@ -4,15 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LeetCode.QuestionBank.Question0741
+namespace LeetCode.QuestionBank.Question1463
 {
-    public class Solution0741_3 : Interface0741
+    public class Solution1463 : Interface1463
     {
-        /// <summary>
-        /// DP
-        /// </summary>
-        /// <param name="grid"></param>
-        /// <returns></returns>
         public int CherryPickup(int[][] grid)
         {
             throw new NotImplementedException();
