282b2aa1af9292b5f6d387464d44ead2388bfca6
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-05-08 09:09:06 +0800

    20240508-090906

---
+++ C#/LeetCode/QuestionBank/Question3127/Interface3127.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3127
+{
+    public interface Interface3127
+    {
+        public bool CanMakeSquare(char[][] grid);
+    }
+}
