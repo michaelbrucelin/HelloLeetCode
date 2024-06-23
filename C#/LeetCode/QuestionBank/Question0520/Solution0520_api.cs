099a7af285df55174dcb6871efe2982b7f8aa7a5
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-06-23 09:57:49 +0800

    20240623-095748

---
+++ C#/LeetCode/QuestionBank/Question0520/Solution0520_api.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0520
+{
+    public class Solution0520_api : Interface0520
+    {
+        public bool DetectCapitalUse(string word)
+        {
+            if (word.Length == 1) return true;
+
+            return Regex.IsMatch("", @"^([a-z]+|[A-Z]+|[A-Z][a-z]*)$");
+        }
+    }
+}
