62fdf34c3507a98506e467a6d45e9d68c08afc08
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-08-14 10:52:11 +0800

    20240814-105210

---
+++ C#/LeetCode/QuestionBank/Question3152/Solution3152_off.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3152
+{
+    public class Solution3152_off : Interface3152
+    {
+        public bool[] IsArraySpecial(int[] nums, int[][] queries)
+        {
+            int len = nums.Length;
+            int[] dp = new int[len]; dp[0] = 1;
+            for (int i = 1; i < len; i++) dp[i] = (((nums[i] ^ nums[i - 1]) & 1) != 0) ? dp[i - 1] + 1 : 1;
+
+            len = queries.Length;
+            bool[] result = new bool[len];
+            for (int i = 0, from, to; i < len; i++)
+            {
+                (from, to) = (queries[i][0], queries[i][1]);
+                result[i] = dp[to] >= to - from + 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question3152/Solution3152_oth.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3152
+{
+    public class Solution3152_oth : Interface3152
+    {
+        public bool[] IsArraySpecial(int[] nums, int[][] queries)
+        {
+            int len = nums.Length;
+            int[] sums = new int[len + 1];
+            for (int i = 1; i < len; i++) sums[i + 1] = sums[i] + (((nums[i] ^ nums[i - 1]) & 1) ^ 1);
+
+            len = queries.Length;
+            bool[] result = new bool[len];
+            for (int i = 0, from, to; i < len; i++)
+            {
+                (from, to) = (queries[i][0], queries[i][1]);
+                result[i] = sums[to + 1] - sums[from + 1] == 0;
+            }
+
+            return result;
+        }
+    }
+}
