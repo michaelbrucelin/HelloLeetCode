d120e94f8ef5873991b47f8cd00bcdd8a0de31d5
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-08-27 10:08:59 +0800

    20240827-100857

---
+++ C#/LeetCode/QuestionBank/Question3134/Interface3134.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3134
+{
+    public interface Interface3134
+    {
+        public int MedianOfUniquenessArray(int[] nums);
+    }
+}
