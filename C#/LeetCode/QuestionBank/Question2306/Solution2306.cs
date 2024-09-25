f80510aed3ca448c851b3e52fc7da63e594d68b1
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-09-25 09:15:13 +0800

    20240925-091511

---
+++ C#/LeetCode/QuestionBank/Question2306/Solution2306.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2306
+{
+    public class Solution2306 : Interface2306
+    {
+        /// <summary>
+        /// 暴力枚举
+        /// 显然会TLE，先写出来，然后再逐步优化
+        /// </summary>
+        /// <param name="ideas"></param>
+        /// <returns></returns>
+        public long DistinctNames(string[] ideas)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
