38743a601209182f9d2109d07dc47561ee407ecd
michaelbrucelin <michaelbrucelin@gmail.com>
2024-12-18 13:29:40 +0800

    20241218-132940

---
+++ C#/LeetCode/QuestionBank/Question3292/Solution3292.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3292
+{
+    public class Solution3292 : Interface3292
+    {
+        public int MinValidStrings(string[] words, string target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
