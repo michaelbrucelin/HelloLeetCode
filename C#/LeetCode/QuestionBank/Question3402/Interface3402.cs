0e6840f27909cec532617245092c15fd9e471949
michaelbrucelin <michaelbrucelin@gmail.com>
2025-01-01 16:41:15 +0800

    20250101-164115

---
+++ C#/LeetCode/QuestionBank/Question3402/Interface3402.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3402
+{
+    public interface Interface3402
+    {
+        public int MinimumOperations(int[][] grid);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question3402/Solution3402.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3402
+{
+    public class Solution3402 : Interface3402
+    {
+        /// <summary>
+        /// 遍历
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <returns></returns>
+        public int MinimumOperations(int[][] grid)
+        {
+            int result = 0, rcnt = grid.Length, ccnt = grid[0].Length;
+            for (int c = 0; c < ccnt; c++) for (int r = 0, last = grid[0][c] - 1; r < rcnt; r++)
+                {
+                    if (grid[r][c] <= last)
+                    {
+                        result += last + 1 - grid[r][c];
+                        last++;
+                    }
+                    else
+                    {
+                        last = grid[r][c];
+                    }
+                }
+
+            return result;
+        }
+    }
+}
