425f6547981272c57ef2665d544ecc8cbb064bd5
michaelbrucelin <michaelbrucelin@gmail.com>
2025-01-02 11:33:56 +0800

    20250102-113356

---
+++ C#/LeetCode/QuestionBank/Question0729/Solution0729_3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0729
+{
+    public class Solution0729_3
+    {
+    }
+
+    public class MyCalendar_3 : Interface0729
+    {
+        /// <summary>
+        /// 跳表
+        /// 逻辑同Solution0729，将线性表改为了跳表
+        /// 
+        /// C#中没有内置跳表，先不写了
+        /// </summary>
+        public MyCalendar_3()
+        {
+
+        }
+
+        public bool Book(int startTime, int endTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
