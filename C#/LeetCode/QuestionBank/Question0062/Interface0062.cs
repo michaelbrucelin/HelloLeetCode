15496f1860c2c1689b147f7bd57fd46f2aab3eb7
michaelbrucelin <michaelbrucelin@gmail.com>
2025-01-15 09:24:41 +0800

    20250115-092440

---
+++ C#/LeetCode/QuestionBank/Question0062/Interface0062.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0062
+{
+    public interface Interface0062
+    {
+        public int UniquePaths(int m, int n);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question0063/Interface0063.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0063
+{
+    public interface Interface0063
+    {
+        public int UniquePathsWithObstacles(int[][] obstacleGrid);
+    }
+}
