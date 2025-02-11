8e4ad5571a23d170ab7d87f041fbf52d7a424e92
michaelbrucelin <michaelbrucelin@gmail.com>
2025-02-11 13:46:37 +0800

    20250211-134637

---
+++ C#/LeetCode/QuestionBank/Question1728/Interface1728.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1728
+{
+    public interface Interface1728
+    {
+        public bool CanMouseWin(string[] grid, int catJump, int mouseJump);
+    }
+}
