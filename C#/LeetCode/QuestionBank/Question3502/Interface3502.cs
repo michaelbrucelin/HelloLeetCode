ec29eb7a76adabeee66d8f85f24b920eff0f6cf3
michaelbrucelin <michaelbrucelin@gmail.com>
2025-04-01 13:15:24 +0800

    20250401-131524

---
+++ C#/LeetCode/QuestionBank/Question3502/Interface3502.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3502
+{
+    public interface Interface3502
+    {
+        public int[] MinCosts(int[] cost);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question3502/Solution3502.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3502
+{
+    public class Solution3502 : Interface3502
+    {
+        /// <summary>
+        /// 遍历
+        /// 遍历找每个位置的前面的最小值
+        /// </summary>
+        /// <param name="cost"></param>
+        /// <returns></returns>
+        public int[] MinCosts(int[] cost)
+        {
+            int len = cost.Length;
+            int[] result = new int[len];
+
+            result[0] = cost[0];
+            for (int i = 1; i < len; i++) result[i] = Math.Min(result[i - 1], cost[i]);
+
+            return result;
+        }
+    }
+}
