242e5b9d308805d7b45ba93d309795a6405740ce
michaelbrucelin <michaelbrucelin@gmail.com>
2025-04-02 10:37:39 +0800

    20250402-103739

---
+++ C#/LeetCode/QuestionBank/Question2874/Interface2874.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2874
+{
+    public interface Interface2874
+    {
+        public long MaximumTripletValue(int[] nums);
+    }
+}
