3c43426297586d03e8ef10414dd86cdbbb217dbd
michaelbrucelin <michaelbrucelin@gmail.com>
2025-04-15 17:37:34 +0800

    20250415-173734

---
+++ C#/LeetCode/QuestionBank/Question0315/Interface0315.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0315
+{
+    public interface Interface0315
+    {
+        public IList<int> CountSmaller(int[] nums);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question0315/Solution0315.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0315
+{
+    public class Solution0315 : Interface0315
+    {
+        public IList<int> CountSmaller(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
