03654a9f7357ca8a92f4c5230bcffa02830e9de0
michaelbrucelin <michaelbrucelin@gmail.com>
2025-04-24 09:43:17 +0800

    20250424-094317

---
+++ C#/LeetCode/QuestionBank/Question3349/Solution3349.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3349
+{
+    public class Solution3349 : Interface3349
+    {
+        /// <summary>
+        /// 遍历
+        /// 遍历查找所有递增区间，找到长度 >= 2k 的区间 或 连续两个长度 >= k 的区间
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public bool HasIncreasingSubarrays(IList<int> nums, int k)
+        {
+            if (k == 1) return true;
+
+            int l1 = 0, r1 = 0, l2 = 0, r2 = 0, cnt = nums.Count;
+            for (int i = 1; i < cnt; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    r2 = i;
+                }
+                else
+                {
+                    if (i - l2 >= (k << 1)) return true;
+                    if (i - l2 >= k && r1 - l1 + 1 >= k) return true;
+                    l1 = l2; r1 = r2; l2 = i;
+                }
+            }
+            if (r2 - l2 + 1 >= (k << 1)) return true;
+            if (r2 - l2 + 1 >= k && r1 - l1 + 1 >= k) return true;
+
+            return false;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question3349/Test3349.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3349
+{
+    public class Test3349
+    {
+        public void Test()
+        {
+            Interface3349 solution = new Solution3349();
+            IList<int> nums; int k;
+            bool result, answer;
+            int id = 0;
+
+            // 1. 
+            nums = [2, 5, 7, 8, 9, 2, 3, 4, 3, 1]; k = 3;
+            answer = true;
+            result = solution.HasIncreasingSubarrays(nums, k);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 2. 
+            nums = [1, 2, 3, 4, 4, 4, 4, 5, 6, 7]; k = 5;
+            answer = false;
+            result = solution.HasIncreasingSubarrays(nums, k);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+        }
+    }
+}
