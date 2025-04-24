2348c79cfa25704eb2a0121ec8d8552f77c26b32
michaelbrucelin <michaelbrucelin@gmail.com>
2025-04-24 10:34:21 +0800

    20250424-103421

---
+++ C#/LeetCode/QuestionBank/Question2221/Interface2221.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2221
+{
+    public interface Interface2221
+    {
+        public int TriangularSum(int[] nums);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question2221/Solution2221.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2221
+{
+    public class Solution2221 : Interface2221
+    {
+        /// <summary>
+        /// 模拟
+        /// 原地模拟
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int TriangularSum(int[] nums)
+        {
+            int len = nums.Length;
+            // for (int i = len - 1; i > 0; i--) for (int j = 0; j < i; j++) nums[j] += nums[j + 1];  // 溢出了
+            for (int i = len - 1; i > 0; i--) for (int j = 0; j < i; j++) nums[j] = (nums[j] + nums[j + 1]) % 10;
+
+            return nums[0];
+        }
+    }
+}
