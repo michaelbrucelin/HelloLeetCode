2a7b8f8ba178482e23001eab4fa596537b0a3818
michaelbrucelin <michaelbrucelin@gmail.com>
2025-05-12 08:25:07 +0800

    20250512-082506

---
+++ C#/LeetCode/QuestionBank/Question0133/Solution0133.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0133
+{
+    public class Solution0133 : Interface0133
+    {
+        /// <summary>
+        /// DFS
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public Node CloneGraph(Node node)
+        {
+            Node _node = new Node(node.val);
+            HashSet<Node> visited= new HashSet<Node>();
+
+            return _node;
+        }
+    }
+}
