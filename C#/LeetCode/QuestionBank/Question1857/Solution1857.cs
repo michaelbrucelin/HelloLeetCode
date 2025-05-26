63a27054e3a650a87165cf11314efb112b0c329e
michaelbrucelin <michaelbrucelin@gmail.com>
2025-05-26 13:41:32 +0800

    20250526-134132

---
+++ C#/LeetCode/QuestionBank/Question1857/Solution1857.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1857
+{
+    public class Solution1857 : Interface1857
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="colors"></param>
+        /// <param name="edges"></param>
+        /// <returns></returns>
+        public int LargestPathValue(string colors, int[][] edges)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
