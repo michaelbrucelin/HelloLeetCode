ec68ac8eb6c4ce1d9ac3aa7dcb701b16b70bb6df
michaelbrucelin <michaelbrucelin@gmail.com>
2025-06-10 08:30:05 +0800

    20250610-083005

---
+++ C#/LeetCode/QuestionBank/Question3442/Interface3442.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3442
+{
+    public interface Interface3442
+    {
+        public int MaxDifference(string s);
+    }
+}
