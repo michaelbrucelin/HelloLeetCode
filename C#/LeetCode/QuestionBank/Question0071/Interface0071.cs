1305df00cefdf4b60219653c13a046893883ce44
michaelbrucelin <michaelbrucelin@gmail.com>
2025-07-05 12:15:12 +0800

    20250705-121511

---
+++ C#/LeetCode/QuestionBank/Question0071/Interface0071.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0071
+{
+    public interface Interface0071
+    {
+        public string SimplifyPath(string path);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question0071/Solution0071.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0071
+{
+    public class Solution0071 : Interface0071
+    {
+        public string SimplifyPath(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
