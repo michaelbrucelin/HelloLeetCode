47fb55f2de8634537aedcac2677b15485d638e2a
michaelbrucelin <michaelbrucelin@gmail.com>
2025-07-08 08:55:09 +0800

    20250708-085509

---
+++ C#/LeetCode/QuestionBank/Question1751/Interface1751.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1751
+{
+    public interface Interface1751
+    {
+        public int MaxValue(int[][] events, int k);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1751/Solution1751.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1751
+{
+    public class Solution1751 : Interface1751
+    {
+        public int MaxValue(int[][] events, int k)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
