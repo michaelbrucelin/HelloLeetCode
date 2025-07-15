7aaf6ec3a65431bc5403ef2163e302f796e263af
michaelbrucelin <michaelbrucelin@gmail.com>
2025-07-16 07:48:03 +0800

    20250716-074803

---
+++ C#/LeetCode/QuestionBank/Question3202/Interface3202.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3202
+{
+    public interface Interface3202
+    {
+        public int MaximumLength(int[] nums, int k);
+    }
+}
