b04ea80545648ed05a76bd718f2feb6153d10680
michaelbrucelin <michaelbrucelin@gmail.com>
2025-07-19 12:08:21 +0800

    20250719-120821

---
+++ C#/LeetCode/QuestionBank/Question1233/Solution1233_3.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1233
+{
+    public class Solution1233_3 : Interface1233
+    {
+        /// <summary>
+        /// 字典树
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <returns></returns>
+        public IList<string> RemoveSubfolders(string[] folder)
+        {
+            if (folder.Length == 1) return folder;
+
+            Trie trie = new Trie();
+            Trie ptr;
+            foreach (string dir in folder)
+            {
+                ptr = trie;
+                foreach (char c in dir)
+                {
+                    if (ptr.Children.ContainsKey(c))
+                    {
+                        if (ptr.Children[c].IsEnd) goto CONTINUE;
+                    }
+                    else
+                    {
+                        ptr.Children.Add(c, new Trie());
+                    }
+                    ptr = ptr.Children[c];
+                }
+                if (!ptr.Children.ContainsKey('/')) ptr.Children.Add('/', new Trie());
+                ptr.Children['/'].IsEnd = true;
+            CONTINUE:;
+            }
+
+            List<string> result = new List<string>();
+            StringBuilder sb = new StringBuilder();
+            dfs(trie.Children['/'], '/', sb);
+            return result;
+
+            void dfs(Trie node, char c, StringBuilder sb)
+            {
+                if (node.IsEnd) { result.Add(sb.ToString()); return; }
+                sb.Append(c);
+                foreach (char _c in node.Children.Keys)
+                {
+                    StringBuilder _sb = new StringBuilder(sb.ToString());
+                    dfs(node.Children[_c], _c, _sb);
+                }
+            }
+        }
+
+        public class Trie
+        {
+            public Trie() { IsEnd = false; Children = new(); }
+            public bool IsEnd { get; set; }
+            public Dictionary<char, Trie> Children { get; set; }
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1233/Test1233.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Utilses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1233
+{
+    public class Test1233
+    {
+        public void Test()
+        {
+            Interface1233 solution = new Solution1233_3();
+            string[] folder;
+            IList<string> result, answer;
+            int id = 0;
+
+            // 1. 
+            folder = ["/a", "/a/b", "/c/d", "/c/d/e", "/c/f"];
+            answer = ["/a", "/c/d", "/c/f"];
+            result = solution.RemoveSubfolders(folder);
+            Console.WriteLine($"{++id,2}: {Utils.CompareArray(result, answer) + ",",-6} result: {Utils.ToString(result)}, answer: {Utils.ToString(answer)}");
+
+            // 2. 
+            folder = ["/a", "/a/b/c", "/a/b/d"];
+            answer = ["/a"];
+            result = solution.RemoveSubfolders(folder);
+            Console.WriteLine($"{++id,2}: {Utils.CompareArray(result, answer) + ",",-6} result: {Utils.ToString(result)}, answer: {Utils.ToString(answer)}");
+
+            // 3. 
+            folder = ["/a/b/c", "/a/b/ca", "/a/b/d"];
+            answer = ["/a/b/c", "/a/b/ca", "/a/b/d"];
+            result = solution.RemoveSubfolders(folder);
+            Console.WriteLine($"{++id,2}: {Utils.CompareArray(result, answer) + ",",-6} result: {Utils.ToString(result)}, answer: {Utils.ToString(answer)}");
+        }
+    }
+}
