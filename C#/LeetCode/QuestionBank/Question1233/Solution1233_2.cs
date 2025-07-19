e2c07abff333b3ff49ff4e4cf5179466a1497c88
michaelbrucelin <michaelbrucelin@gmail.com>
2025-07-19 11:11:52 +0800

    20250719-111152

---
+++ C#/LeetCode/QuestionBank/Question1233/Solution1233_2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1233
+{
+    public class Solution1233_2 : Interface1233
+    {
+        /// <summary>
+        /// 排序
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <returns></returns>
+        public IList<string> RemoveSubfolders(string[] folder)
+        {
+            if (folder.Length == 1) return folder;
+
+            List<string> result = new List<string>();
+            Array.Sort(folder);
+            result.Add(folder[0]);
+            int len = folder.Length;
+            for (int i = 1; i < len; i++) if (!$"{folder[i]}/".StartsWith($"{result[^1]}/")) result.Add(folder[i]);
+
+            return result;
+        }
+    }
+}
