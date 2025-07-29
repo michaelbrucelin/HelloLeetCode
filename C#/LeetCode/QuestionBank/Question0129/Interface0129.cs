59ebca59cf9f86df980ca8cb16029f482a943cd5
michaelbrucelin <michaelbrucelin@gmail.com>
2025-07-30 07:56:36 +0800

    20250730-075636

---
+++ C#/LeetCode/QuestionBank/Question0129/Interface0129.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0129
+{
+    public interface Interface0129
+    {
+        public int SumNumbers(TreeNode root);
+    }
+}
