681451f2a2eebbcbba86a12648260f515ab46a1e
michaelbrucelin <michaelbrucelin@gmail.com>
2025-08-03 16:36:22 +0800

    20250803-163622

---
+++ C#/LeetCode/QuestionBank/Question2561/Interface2561.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2561
+{
+    public interface Interface2561
+    {
+        public long MinCost(int[] basket1, int[] basket2);
+    }
+}
