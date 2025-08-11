6fae3c822a21fc25a85c1edf79b62d51f52b75bd
michaelbrucelin <michaelbrucelin@gmail.com>
2025-08-11 10:13:52 +0800

    20250811-101351

--- C#/LeetCode/QuestionBank/Question0231/Solution0231.cs
+++ C#/LeetCode/QuestionBank/Question0231/Solution0231.cs
@@ -31,7 +31,8 @@ namespace LeetCode.QuestionBank.Question0231
             return IsPowerOfTwo2(n >> 1);
         }
 
-        private static readonly HashSet<int> pow2s = new HashSet<int>() { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824 };
+        private static readonly HashSet<int> pow2s = new HashSet<int>() { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536,
+            131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824 };
         public bool IsPowerOfTwo3(int n)
         {
             return pow2s.Contains(n);
