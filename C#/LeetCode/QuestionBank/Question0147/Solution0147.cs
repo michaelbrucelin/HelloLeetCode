62711c643e8ac6147f086398f350a969505af1a8
michaelbrucelin <michaelbrucelin@gmail.com>
2025-08-19 08:22:36 +0800

    20250819-082236

---
+++ C#/LeetCode/QuestionBank/Question0147/Solution0147.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0147
+{
+    public class Solution0147 : Interface0147
+    {
+        public ListNode InsertionSortList(ListNode head)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
