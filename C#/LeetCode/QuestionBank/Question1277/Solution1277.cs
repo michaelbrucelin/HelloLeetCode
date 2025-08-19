9edb50f561073aa3dc2882446281a7f994a8e9c5
michaelbrucelin <michaelbrucelin@gmail.com>
2025-08-20 07:56:05 +0800

    20250820-075605

---
+++ C#/LeetCode/QuestionBank/Question1277/Solution1277.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1277
+{
+    public class Solution1277 : Interface1277
+    {
+        public int CountSquares(int[][] matrix)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
