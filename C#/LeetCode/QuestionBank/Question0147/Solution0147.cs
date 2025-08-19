902c3b4f7b0b86a809c7d28b425e3f84fef1e016
michaelbrucelin <michaelbrucelin@gmail.com>
2025-08-19 08:47:57 +0800

    20250819-084757

--- C#/LeetCode/QuestionBank/Question0147/Solution0147.cs
+++ C#/LeetCode/QuestionBank/Question0147/Solution0147.cs
@@ -8,9 +8,27 @@ namespace LeetCode.QuestionBank.Question0147
 {
     public class Solution0147 : Interface0147
     {
+        /// <summary>
+        /// 三指针
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
         public ListNode InsertionSortList(ListNode head)
         {
-            throw new NotImplementedException();
+            if (head == null || head.next == null) return head;
+
+            ListNode dummy = new ListNode(int.MinValue, head);
+            ListNode pl, pr = head, temp;
+            while (pr != null)
+            {
+                while (pr.next != null && pr.next.val >= pr.val) pr = pr.next;
+                if (pr.next == null) break;
+                pl = dummy;
+                while (pl.next.val < pr.next.val) pl = pl.next;
+                temp = pr.next.next; pr.next.next = pl.next; pl.next = pr.next; pr.next = temp;
+            }
+
+            return dummy.next;
         }
     }
 }
