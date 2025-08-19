33962311eb397c6d85a637dd1fc6ef22e69c5d4d
michaelbrucelin <michaelbrucelin@gmail.com>
2025-08-19 08:09:02 +0800

    20250819-080901

---
+++ C#/LeetCode/QuestionBank/Question2348/Interface2348.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2348
+{
+    public interface Interface2348
+    {
+        public long ZeroFilledSubarray(int[] nums);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question2348/Solution2348.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2348
+{
+    public class Solution2348 : Interface2348
+    {
+        /// <summary>
+        /// 双指针 + 数学
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public long ZeroFilledSubarray(int[] nums)
+        {
+            long result = 0;
+            int pl = 0, pr, len = nums.Length;
+            while (pl < len)
+            {
+                while (pl < len && nums[pl] != 0) pl++;
+                if (pl == len) break;
+                pr = pl;
+                while (pr + 1 < len && nums[pr + 1] == 0) pr++;
+                result += (1L * (pr - pl + 1) * (pr - pl + 2)) >> 1;
+                pl = pr + 2;
+            }
+
+            return result;
+        }
+    }
+}
