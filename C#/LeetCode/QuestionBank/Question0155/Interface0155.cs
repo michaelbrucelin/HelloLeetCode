d735115ea8c9f3c67e4b170a42814f45008d2c1f
michaelbrucelin <michaelbrucelin@gmail.com>
2025-08-20 08:55:10 +0800

    20250820-085510

---
+++ C#/LeetCode/QuestionBank/Question0155/Interface0155.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0155
+{
+    /// <summary>
+    /// Your MinStack object will be instantiated and called as such:
+    /// MinStack obj = new MinStack();
+    /// obj.Push(val);
+    /// obj.Pop();
+    /// int param_3 = obj.Top();
+    /// int param_4 = obj.GetMin();
+    /// </summary>
+    public interface Interface0155
+    {
+        // public MinStack() { }
+
+        public void Push(int val);
+
+        public void Pop();
+
+        public int Top();
+
+        public int GetMin();
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question0164/Interface0164.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0164
+{
+    public interface Interface0164
+    {
+        public int MaximumGap(int[] nums);
+    }
+}
