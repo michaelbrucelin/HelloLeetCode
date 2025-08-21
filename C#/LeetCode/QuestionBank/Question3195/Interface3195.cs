766c67317e3e7bfb6f474f78ed47771dd392912e
michaelbrucelin <michaelbrucelin@gmail.com>
2025-08-22 07:42:21 +0800

    20250822-074221

---
+++ C#/LeetCode/QuestionBank/Question3195/Interface3195.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3195
+{
+    public interface Interface3195
+    {
+        public int MinimumArea(int[][] grid);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question3197/Interface3197.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3197
+{
+    public interface Interface3197
+    {
+        public int MinimumSum(int[][] grid);
+    }
+}
