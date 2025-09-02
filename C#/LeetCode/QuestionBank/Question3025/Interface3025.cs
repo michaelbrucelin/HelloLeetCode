f65360a66e5f8ee3b3daeb61bd090cafadda06b3
michaelbrucelin <michaelbrucelin@gmail.com>
2025-09-02 08:05:17 +0800

    20250902-080517

---
+++ C#/LeetCode/QuestionBank/Question3025/Interface3025.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3025
+{
+    public interface Interface3025
+    {
+        public int NumberOfPairs(int[][] points);
+    }
+}
