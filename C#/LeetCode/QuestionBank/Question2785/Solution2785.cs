ba45061e05f84c76ea7f3945201907c14366fa42
michaelbrucelin <michaelbrucelin@gmail.com>
2025-09-11 08:09:35 +0800

    20250911-080934

---
+++ C#/LeetCode/QuestionBank/Question2785/Solution2785.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2785
+{
+    public class Solution2785 : Interface2785
+    {
+        /// <summary>
+        /// 模拟
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string SortVowels(string s)
+        {
+            HashSet<char> vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
+            List<char> list = [];
+            List<int> pos = [];
+            int x = s.Length;
+            for (int i = 0; i < x; i++) if (vowels.Contains(s[i]))
+                {
+                    list.Add(s[i]); pos.Add(i);
+                }
+            list.Sort();
+
+            char[] result = [.. s];
+            x = 0;
+            foreach (int i in pos) result[i] = list[x++];
+            return new string(result);
+        }
+    }
+}
