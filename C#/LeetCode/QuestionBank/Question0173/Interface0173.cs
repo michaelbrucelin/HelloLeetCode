7cd8dbc06976f15eb70e8059485399173e82efd4
michaelbrucelin <michaelbrucelin@gmail.com>
2025-09-17 08:37:50 +0800

    20250917-083750

---
+++ C#/LeetCode/QuestionBank/Question0173/Interface0173.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0173
+{
+    /// <summary>
+    /// Your BSTIterator object will be instantiated and called as such:
+    /// BSTIterator obj = new BSTIterator(root);
+    /// int param_1 = obj.Next();
+    /// bool param_2 = obj.HasNext();
+    /// </summary>
+    public interface Interface0173
+    {
+        // public BSTIterator(TreeNode root){}
+
+        public int Next();
+
+        public bool HasNext();
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question0173/Solution0173.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0173
+{
+    public class Solution0173
+    {
+    }
+
+    public class BSTIterator : Interface0173
+    {
+        public BSTIterator(TreeNode root)
+        {
+
+        }
+
+        public bool HasNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Next()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
