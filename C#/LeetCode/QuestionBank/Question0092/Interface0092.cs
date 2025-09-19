0bb0b0d881cf472595625f928e505fe676bca165
michaelbrucelin <michaelbrucelin@gmail.com>
2025-09-19 08:30:00 +0800

    20250919-083000

---
+++ C#/LeetCode/QuestionBank/Question0092/Interface0092.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0092
+{
+    public interface Interface0092
+    {
+        public ListNode ReverseBetween(ListNode head, int left, int right);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question0113/Interface0113.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0113
+{
+    public interface Interface0113
+    {
+        public IList<IList<int>> PathSum(TreeNode root, int targetSum);
+    }
+}
