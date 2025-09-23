c9fa31036db6305076d1866c0497feeef4701902
michaelbrucelin <michaelbrucelin@gmail.com>
2025-09-23 08:27:10 +0800

    20250923-082710

---
+++ C#/LeetCode/QuestionBank/Question0079/Solution0079.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0079
+{
+    public class Solution0079 : Interface0079
+    {
+        /// <summary>
+        /// 回溯
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public bool Exist(char[][] board, string word)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
