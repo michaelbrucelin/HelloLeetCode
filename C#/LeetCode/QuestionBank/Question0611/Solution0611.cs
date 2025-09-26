6a5b31f02730063708fc3c52e1f954f55cb47afd
michaelbrucelin <michaelbrucelin@gmail.com>
2025-09-26 09:00:28 +0800

    20250926-090028

---
+++ C#/LeetCode/QuestionBank/Question0611/Solution0611.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0611
+{
+    public class Solution0611 : Interface0611
+    {
+        /// <summary>
+        /// 排序 + 二分
+        /// 排序后，两层循环枚举两条小边，二分查找大边的边界
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int TriangleNumber(int[] nums)
+        {
+            if (nums.Length < 3) return 0;
+
+            Array.Sort(nums);
+            int result = 0, start = 0, len = nums.Length;
+            while (start < len && nums[start] == 0) start++;
+            if (len - start < 2) return 0;
+            for (int i = start, k; i < len; i++) for (int j = i + 1; j < len; j++)
+                {
+                    k = BinarySearch(j + 1, nums[i] + nums[j]);
+                    result += k - j;
+                }
+
+            return result;
+
+            int BinarySearch(int left, int target)
+            {
+                int _result = left - 1, right = len - 1, mid;
+                while (left <= right)
+                {
+                    mid = left + ((right - left) >> 1);
+                    if (nums[mid] < target) { _result = mid; left = mid + 1; } else { right = mid - 1; }
+                }
+
+                return _result;
+            }
+        }
+
+        /// <summary>
+        /// 逻辑同TriangleNumber()，二分的时候增加了剪枝
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int TriangleNumber2(int[] nums)
+        {
+            if (nums.Length < 3) return 0;
+
+            Array.Sort(nums);
+            int result = 0, start = 0, len = nums.Length;
+            while (start < len && nums[start] == 0) start++;
+            if (len - start < 2) return 0;
+            for (int i = start; i < len; i++) for (int j = i + 1, k = i + 2; j < len; j++)
+                {
+                    k = BinarySearch(k, nums[i] + nums[j]);
+                    result += k - j;
+                }
+
+            return result;
+
+            int BinarySearch(int left, int target)
+            {
+                int _result = left - 1, right = len - 1, mid;
+                while (left <= right)
+                {
+                    mid = left + ((right - left) >> 1);
+                    if (nums[mid] < target) { _result = mid; left = mid + 1; } else { right = mid - 1; }
+                }
+
+                return _result;
+            }
+        }
+
+        /// <summary>
+        /// 逻辑依然同TriangleNumber()，题目的数据范围不大，将二分退化为三指针
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int TriangleNumber3(int[] nums)
+        {
+            if (nums.Length < 3) return 0;
+
+            Array.Sort(nums);
+            int result = 0, start = 0, len = nums.Length;
+            while (start < len && nums[start] == 0) start++;
+            if (len - start < 2) return 0;
+            for (int i = start, sum; i < len; i++) for (int j = i + 1, k = i + 1; j < len; j++)
+                {
+                    sum = nums[i] + nums[j];
+                    while (k + 1 < len && nums[k + 1] < sum) k++;
+                    result += k - j;
+                }
+
+            return result;
+        }
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question0611/Solution0611_2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0611
+{
+    public class Solution0611_2 : Interface0611
+    {
+        /// <summary>
+        /// 数据压缩
+        /// 逻辑同Solution0611，先数据压缩（相同的记录数量），然后再执行Solution0611的逻辑
+        /// 数据范围不大，可以使用计数排序一次性搞定排序与计数
+        /// 
+        /// 没时间，先不写了，有时间再写
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int TriangleNumber(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
