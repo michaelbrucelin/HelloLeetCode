6fdf25677d9224ced9380b644be2b706c924b1a4
michaelbrucelin <michaelbrucelin@gmail.com>
2025-10-07 12:00:29 +0800

    20251007-120029

---
+++ C#/LeetCode/QuestionBank/Question0417/Interface0417.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0417
+{
+    public interface Interface0417
+    {
+        public IList<IList<int>> PacificAtlantic(int[][] heights);
+    }
+}
