7f107b76c183ed8334c0615bab33cf9a411726f7
michaelbrucelin <michaelbrucelin@gmail.com>
2025-10-13 09:30:16 +0800

    20251013-093016

---
+++ C#/LeetCode/QuestionBank/Question3539/Interface3539.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3539
+{
+    public interface Interface3539
+    {
+        public int MagicalSum(int m, int k, int[] nums);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question3539/Solution3539.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3539
+{
+    public class Solution3539 : Interface3539
+    {
+        public int MagicalSum(int m, int k, int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
