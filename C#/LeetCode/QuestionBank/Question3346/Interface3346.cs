bd59dc3a923b74374209d20826ade4efa6d185bf
michaelbrucelin <michaelbrucelin@gmail.com>
2025-10-21 07:25:33 +0800

    20251021-072532

---
+++ C#/LeetCode/QuestionBank/Question3346/Interface3346.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question3346
+{
+    public interface Interface3346
+    {
+        public int MaxFrequency(int[] nums, int k, int numOperations);
+    }
+}
