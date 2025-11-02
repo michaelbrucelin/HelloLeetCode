3bab0266c54f99ba56e9cd3f2a12535be9086bd5
michaelbrucelin <michaelbrucelin@gmail.com>
2025-11-03 07:05:42 +0800

    20251103-070542

---
+++ C#/LeetCode/QuestionBank/Question1578/Interface1578.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1578
+{
+    public interface Interface1578
+    {
+        public int MinCost(string colors, int[] neededTime);
+    }
+}
---
+++ C#/LeetCode/QuestionBank/Question1578/Solution1578.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1578
+{
+    public class Solution1578 : Interface1578
+    {
+        public int MinCost(string colors, int[] neededTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
