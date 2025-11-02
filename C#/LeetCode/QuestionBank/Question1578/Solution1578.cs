bc61723ce528889e971d8d0a55774396134f2ee3
michaelbrucelin <michaelbrucelin@gmail.com>
2025-11-03 07:40:40 +0800

    20251103-074040

--- C#/LeetCode/QuestionBank/Question1578/Solution1578.cs
+++ C#/LeetCode/QuestionBank/Question1578/Solution1578.cs
@@ -8,9 +8,26 @@ namespace LeetCode.QuestionBank.Question1578
 {
     public class Solution1578 : Interface1578
     {
+        /// <summary>
+        /// 双指针
+        /// 连续相同颜色，保留时间最长的即可
+        /// </summary>
+        /// <param name="colors"></param>
+        /// <param name="neededTime"></param>
+        /// <returns></returns>
         public int MinCost(string colors, int[] neededTime)
         {
-            throw new NotImplementedException();
+            int result = 0, p1 = 0, p2, sum, max, len = colors.Length;
+            while (p1 < len)
+            {
+                p2 = p1;
+                while (p2 + 1 < len && colors[p2 + 1] == colors[p1]) p2++;
+                sum = max = neededTime[p1];
+                while (++p1 <= p2) { sum += neededTime[p1]; max = Math.Max(max, neededTime[p1]); }
+                result += sum - max;
+            }
+
+            return result;
         }
     }
 }
