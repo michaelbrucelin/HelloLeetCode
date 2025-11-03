cf714927123f5123e60f8c58298947e2ee793e52
michaelbrucelin <michaelbrucelin@gmail.com>
2025-11-03 16:13:53 +0800

    20251103-161353

---
+++ C#/LeetCode/QuestionBank/Question0172/Solution0172_2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0172
+{
+    public class Solution0172_2 : Interface0172
+    {
+        /// <summary>
+        /// 遍历
+        /// 逻辑同Solution0172，稍加改动，具体见代码
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int TrailingZeroes(int n)
+        {
+            if (n == 0) return 0;
+
+            int result = 0, k = 1;
+            while ((k *= 5) <= n) result += n / k;
+
+            return result;
+        }
+    }
+}
