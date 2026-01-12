ead67db609f737d0cbcf352a082c59f914996fbe
michaelbrucelin <michaelbrucelin@gmail.com>
2026-01-12 09:49:19 +0800

    20260112-094919

--- C#/LeetCode/QuestionBank/Question0912/Solution0912.cs
+++ C#/LeetCode/QuestionBank/Question0912/Solution0912.cs
@@ -35,5 +35,33 @@ namespace LeetCode.QuestionBank.Question0912
                 for (int i = l; i <= r; i++) nums[i] = cache[p++];
             }
         }
+
+        /// <summary>
+        /// 逻辑同SortArray()，节省点内存
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] SortArray2(int[] nums)
+        {
+            int[] cache = new int[nums.Length];
+            _SortArray(0, nums.Length - 1);
+            return nums;
+
+            void _SortArray(int l, int r)
+            {
+                if (l >= r) return;
+                int mid = l + ((r - l) >> 1);
+                _SortArray(l, mid);
+                _SortArray(mid + 1, r);
+
+                int p = 0, pl = l, pr = mid + 1;
+                while (pl <= mid && pr <= r) if (nums[pl] <= nums[pr]) cache[p++] = nums[pl++]; else cache[p++] = nums[pr++];
+                while (pl <= mid) cache[p++] = nums[pl++];
+                while (pr <= r) cache[p++] = nums[pr++];
+
+                p = 0;
+                for (int i = l; i <= r; i++) nums[i] = cache[p++];
+            }
+        }
     }
 }
---
+++ C#/LeetCode/QuestionBank/Question0912/Solution0912_2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0912
+{
+    public class Solution0912_2 : Interface0912
+    {
+        /// <summary>
+        /// 基数排序
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] SortArray(int[] nums)
+        {
+            List<int>[] pos = new List<int>[10], neg = new List<int>[10];
+            List<int>[] _pos = new List<int>[10], _neg = new List<int>[10];
+            for (int i = 0; i < 10; i++) { pos[i] = []; neg[i] = []; }
+
+            // >=0
+            bool flag = false; int radix = 1, radix_next = 10;
+            foreach (int num in nums) if (num >= 0)
+                {
+                    pos[num % 10].Add(num); if (num >= radix_next) flag = true;
+                }
+            while (flag)
+            {
+                flag = false; radix *= 10; radix_next *= 10;
+                for (int i = 0; i < 10; i++) _pos[i] = [];
+                for (int i = 0; i < 10; i++) foreach (int num in pos[i])
+                    {
+                        _pos[num / radix % 10].Add(num); if (num >= radix_next) flag = true;
+                    }
+                for (int i = 0; i < 10; i++) pos[i] = _pos[i];
+            }
+            // <0
+            flag = false; radix = 1; radix_next = 10;
+            foreach (int num in nums) if (num < 0)
+                {
+                    neg[(-num) % 10].Add(-num); if (-num >= radix_next) flag = true;
+                }
+            while (flag)
+            {
+                flag = false; radix *= 10; radix_next *= 10;
+                for (int i = 0; i < 10; i++) _neg[i] = [];
+                for (int i = 0; i < 10; i++) foreach (int num in neg[i])
+                    {
+                        _neg[num / radix % 10].Add(num); if (num >= radix_next) flag = true;
+                    }
+                for (int i = 0; i < 10; i++) neg[i] = _neg[i];
+            }
+
+            int p = 0;
+            for (int i = 9; i >= 0; i--) for (int j = neg[i].Count - 1; j >= 0; j--) nums[p++] = -neg[i][j];
+            for (int i = 0; i < 10; i++) for (int j = 0; j < pos[i].Count; j++) nums[p++] = pos[i][j];
+            return nums;
+        }
+    }
+}
