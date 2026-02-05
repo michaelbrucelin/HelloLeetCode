939160c6bd2bf02690849d4e327ba219fc070f62
michaelbrucelin <michaelbrucelin@gmail.com>
2026-02-05 11:04:27 +0800

    20260205-110426

--- C#/LeetCode/QuestionBank/Question3465/Solution3465.sql
+++ C#/LeetCode/QuestionBank/Question3465/Solution3465.sql
@@ -5,3 +5,12 @@ WHERE    [description] COLLATE Latin1_General_CS_AS like '% SN[0-9][0-9][0-9][0-
       OR [description] COLLATE Latin1_General_CS_AS like '% SN[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]'
       OR [description] COLLATE Latin1_General_CS_AS like   'SN[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]'
 ORDER BY product_id
+
+-- OR
+
+SELECT product_id, product_name, [description]
+FROM products
+WHERE PATINDEX('%[ ]SN[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][ ]%',
+               ' ' + [description] + ' ' COLLATE Latin1_General_CS_AS
+              ) > 0
+ORDER BY product_id
