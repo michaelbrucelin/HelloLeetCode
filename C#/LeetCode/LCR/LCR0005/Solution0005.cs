ea651d6e635ade20eb5bd17342201f748a9a87f1
michaelbrucelin <michaelbrucelin@gmail.com>
2026-02-16 12:58:42 +0800

    20260216-125842

--- C#/LeetCode/LCR/LCR0005/Solution0005.cs
+++ C#/LeetCode/LCR/LCR0005/Solution0005.cs
@@ -29,5 +29,29 @@ namespace LeetCode.LCR.LCR0005
 
             return result;
         }
+
+        /// <summary>
+        /// 逻辑同MaxProduct()，略加优化
+        /// </summary>
+        /// <param name="words"></param>
+        /// <returns></returns>
+        public int MaxProduct2(string[] words)
+        {
+            int result = 0, len = words.Length;
+            Dictionary<int, int> masks = new Dictionary<int, int>();
+            for (int i = 0, mask, _len; i < len; i++)
+            {
+                mask = 0; _len = words[i].Length;
+                for (int j = 0; j < _len; j++) mask |= 1 << (words[i][j] - 'a');
+                if (masks.TryGetValue(mask, out int val)) masks[mask] = Math.Max(val, _len); else masks.Add(mask, _len);
+            }
+
+            foreach (int mask1 in masks.Keys) foreach (int mask2 in masks.Keys)
+                {
+                    if ((mask1 & mask2) == 0) result = Math.Max(result, masks[mask1] * masks[mask2]);
+                }
+
+            return result;
+        }
     }
 }
