6422ddece40f40078c988d3452e371fd64f7f480
michaelbrucelin <michaelbrucelin@gmail.com>
2026-03-08 11:31:20 +0800

    20260308-113119

---
+++ C#/LeetCode/QuestionBank/Question1980/Solution1980_oth_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1980
+{
+    public class Solution1980_oth_2 : Interface1980
+    {
+        public string FindDifferentBinaryString(string[] nums)
+        {
+            const int SUM = '0' + '1';
+            int n = nums.Length;
+            char[] buffer = new char[n];
+            for (int i = 0; i < n; i++) buffer[i] = (char)(SUM - nums[i][i]);
+
+            return new string(buffer);
+        }
+    }
+}
