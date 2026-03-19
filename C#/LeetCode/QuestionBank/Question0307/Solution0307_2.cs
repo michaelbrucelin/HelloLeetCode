599b694b6f566d37a5fd2d023f66105212ec4d90
michaelbrucelin <michaelbrucelin@gmail.com>
2026-03-19 17:19:15 +0800

    20260319-171914

---
+++ C#/LeetCode/QuestionBank/Question0307/Solution0307_2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0307
+{
+    public class Solution0307_2
+    {
+    }
+
+    public class NumArray_2 : Interface0307
+    {
+        /// <summary>
+        /// 树状数组
+        /// 逻辑与Solution0307一样，优化了树状数组的初始化过程
+        /// </summary>
+        /// <param name="nums"></param>
+        public NumArray_2(int[] nums)
+        {
+            len = nums.Length;
+            bit = new int[len + 1];
+            ori = new int[len];
+            build(nums);
+        }
+
+        private int[] bit;
+        private int[] ori;
+        private int len;
+
+        public void Update(int index, int val)
+        {
+            add(index + 1, val - ori[index]);
+            ori[index] = val;
+        }
+
+        public int SumRange(int left, int right)
+        {
+            return sum(right + 1) - sum(left);
+        }
+
+        private void build(int[] nums)
+        {
+            for (int i = 0, j; i < len; i++)
+            {
+                ori[i] = nums[i];
+                bit[i + 1] += nums[i];
+                if ((j = i + 1 + ((i + 1) & (-i - 1))) <= len) bit[j] += bit[i + 1];
+            }
+        }
+
+        private void add(int idx, int val)
+        {
+            while (idx <= len)
+            {
+                bit[idx] += val;
+                idx += idx & -idx;
+            }
+        }
+
+        private int sum(int idx)
+        {
+            int sum = 0;
+            while (idx > 0)
+            {
+                sum += bit[idx];
+                idx -= idx & -idx;
+            }
+            return sum;
+        }
+    }
+}
--- C#/LeetCode/QuestionBank/Question0307/Test0307.cs
+++ C#/LeetCode/QuestionBank/Question0307/Test0307.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using _NumArray = LeetCode.QuestionBank.Question0307.NumArray;
+using _NumArray = LeetCode.QuestionBank.Question0307.NumArray_2;
 
 namespace LeetCode.QuestionBank.Question0307
 {
@@ -21,6 +21,24 @@ namespace LeetCode.QuestionBank.Question0307
             opts = ["NumArray", "sumRange", "update", "sumRange"];
             args = [[1, 3, 5], [0, 2], [1, 2], [0, 2]];
             answer = ["null", "9", "null", "8"];
+            n = opts.Length;
+            solution = new _NumArray(args[0]);
+            for (int i = 1; i < n; i++) switch (opts[i])
+                {
+                    case "update": solution.Update(args[i][0], args[i][1]); break;
+                    case "sumRange":
+                        result = solution.SumRange(args[i][0], args[i][1]);
+                        Console.WriteLine($"{id,2}-{id1++,2}: {(result == Convert.ToInt32(answer[i])) + ",",-6} result: {result}, answer: {answer[i]}");
+                        break;
+                    default: break;
+                }
+
+            // 2. 
+            Console.WriteLine();
+            id++; id1 = 1;
+            opts = ["NumArray", "update", "update", "update", "sumRange", "update", "sumRange", "update", "sumRange", "sumRange", "update"];
+            args = [[7, 2, 7, 2, 0], [4, 6], [0, 2], [0, 9], [4, 4], [3, 8], [0, 4], [4, 1], [0, 3], [0, 4], [0, 4]];
+            answer = ["null", "null", "null", "null", "6", "null", "32", "null", "26", "27", "null"];
             n = opts.Length;
             solution = new _NumArray(args[0]);
             for (int i = 1; i < n; i++) switch (opts[i])
