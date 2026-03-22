b2a9ce8c020cf3c79c2d322c273984da3c24651e
michaelbrucelin <michaelbrucelin@gmail.com>
2026-03-22 16:55:27 +0800

    20260322-165526

--- C#/LeetCode/QuestionBank/Question1998/Solution1998.cs
+++ C#/LeetCode/QuestionBank/Question1998/Solution1998.cs
@@ -11,34 +11,65 @@ namespace LeetCode.QuestionBank.Question1998
         /// <summary>
         /// 并查集 + 堆排序
         /// 并查集找出可以互换位置的组，每个组内部排序，看结果和整体排序的结果是否一致即可
+        /// 具体解释见Solution1269_oth.md
         /// </summary>
         /// <param name="nums"></param>
         /// <returns></returns>
         public bool GcdSort(int[] nums)
         {
-            int len = nums.Length;
-            int[] uf = new int[len], height = new int[len];
-            for (int i = 1; i < len; i++) uf[i] = i;
-            
+            if (nums.Length == 1) return true;
 
-            throw new NotImplementedException();
+            int max = nums[0], len = nums.Length;
+            for (int i = 1; i < len; i++) max = Math.Max(max, nums[i]);
+            List<int> primes = get_primes(max + 1);
 
-            static int gcd(int x, int y)
+            Dictionary<int, int> uf = new Dictionary<int, int>(), height = new Dictionary<int, int>();
+            for (int i = 0, num, x; i < len; i++)
             {
-                if (x == y) return x;
+                num = x = nums[i];
+                if (!uf.ContainsKey(num)) { uf.Add(num, num); height.Add(num, 0); }
+                foreach (int prime in primes)
+                {
+                    if (prime > x) break;
+                    if (x % prime == 0) union(num, prime);
+                    while (x % prime == 0) x /= prime;
+                }
+            }
 
-                int move = 0;
-                while (x != y) switch ((x & 1, y & 1))
+            Dictionary<int, List<int>> map = new Dictionary<int, List<int>>();
+            for (int i = 0, key; i < len; i++)
+            {
+                key = find(nums[i]);
+                if (map.TryGetValue(key, out List<int> list)) list.Add(i); else map.Add(key, [i]);
+            }
+
+            int[] _nums = new int[len];
+            PriorityQueue<int, int> minpq = new PriorityQueue<int, int>();
+            foreach (List<int> list in map.Values)
+            {
+                foreach (int id in list) minpq.Enqueue(nums[id], nums[id]);
+                foreach (int id in list) _nums[id] = minpq.Dequeue();
+            }
+
+            Array.Sort(nums);
+            for (int i = 0; i < len; i++) if (_nums[i] != nums[i]) return false;
+            return true;
+
+            static List<int> get_primes(int n)
+            {
+                List<int> result = new List<int>();
+                bool[] mask = new bool[n]; Array.Fill(mask, true);
+                for (int i = 2; i < n; i++)
+                {
+                    if (mask[i]) result.Add(i);
+                    for (int j = 0; j < result.Count && i * result[j] < n; j++)
                     {
-                        case (0, 0): x >>= 1; y >>= 1; move++; break;
-                        case (0, 1): x >>= 1; break;
-                        case (1, 0): y >>= 1; break;
-                        default:  // (1, 1)
-                            if (x > y) x = (x - y) >> 1; else y = (y - x) >> 1;
-                            break;
+                        mask[i * result[j]] = false;
+                        if (i % result[j] == 0) break;
                     }
+                }
 
-                return x << move;
+                return result;
             }
 
             void union(int x, int y)
@@ -57,6 +88,12 @@ namespace LeetCode.QuestionBank.Question1998
 
             int find(int x)
             {
+                if (!uf.ContainsKey(x))
+                {
+                    uf.Add(x, x); height.Add(x, 0);
+                    return x;
+                }
+
                 int f = x;
                 while (uf[f] != f) f = uf[f];
                 int i = x, j;
---
+++ C#/LeetCode/QuestionBank/Question1998/Test1998.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1998
+{
+    public class Test1998
+    {
+        public void Test()
+        {
+            Interface1998 solution = new Solution1998();
+            int[] nums;
+            bool result, answer;
+            int id = 0;
+
+            // 1. 
+            nums = [7, 21, 3];
+            answer = true;
+            result = solution.GcdSort(nums);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 2. 
+            nums = [5, 2, 6, 2];
+            answer = false;
+            result = solution.GcdSort(nums);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 3. 
+            nums = [10, 5, 9, 3, 15];
+            answer = true;
+            result = solution.GcdSort(nums);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+        }
+    }
+}
