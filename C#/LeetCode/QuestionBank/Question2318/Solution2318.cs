199aa283ad58eda955bfdca547e46c33a1d65cdb
michaelbrucelin <michaelbrucelin@gmail.com>
2026-04-19 15:08:47 +0800

    20260419-150847

--- C#/LeetCode/QuestionBank/Question2318/Solution2318.cs
+++ C#/LeetCode/QuestionBank/Question2318/Solution2318.cs
@@ -16,9 +16,21 @@ namespace LeetCode.QuestionBank.Question2318
         /// <returns></returns>
         public int DistinctSequences(int n)
         {
+            if (n == 1) return 6;
+
             int MOD = (int)1e9 + 7;
             int[,,] dp = new int[n + 1, 7, 7];
-
+            for (int i = 1; i <= 6; i++) for (int j = 1; j <= 6; j++) if (i != j && (i % 2 != 0 || j % 2 != 0) && (i % 3 != 0 || j % 3 != 0)) dp[2, i, j] = 1;
+            for (int _n = 3; _n <= n; _n++)
+            {
+                for (int i = 1; i <= 6; i++) for (int j = 1; j <= 6; j++) if (i != j && (i % 2 != 0 || j % 2 != 0) && (i % 3 != 0 || j % 3 != 0))
+                        {
+                            for (int k = 1; k <= 6; k++) if (k != j)
+                                {
+                                    dp[_n, i, j] = (dp[_n, i, j] + dp[_n - 1, k, i]) % MOD;
+                                }
+                        }
+            }
 
             int result = 0;
             for (int i = 1; i <= 6; i++) for (int j = 1; j <= 6; j++) result = (result + dp[n, i, j]) % MOD;
