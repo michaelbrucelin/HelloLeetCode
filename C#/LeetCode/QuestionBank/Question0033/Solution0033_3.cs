9a8a183c0352e9d9d703805004b5e928091b0ec5
michaelbrucelin <michaelbrucelin@gmail.com>
2026-05-22 09:54:26 +0800

    20260522-095425

---
+++ C#/LeetCode/QuestionBank/Question0033/Solution0033_3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0033
+{
+    public class Solution0033_3 : Interface0033
+    {
+        /// <summary>
+        /// 二分
+        /// 1. 先二分找出数组最小值的位置
+        /// 2. 然后二分查找目标
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int Search(int[] nums, int target)
+        {
+            if (nums.Length == 1) return nums[0] == target ? 0 : -1;
+
+            int minid = 0, low = 0, high = nums.Length - 1, mid;
+            if (nums[0] > nums[^1])
+            {
+                while (low <= high)
+                {
+                    mid = low + ((high - low) >> 1);
+                    if (nums[mid] >= nums[0]) low = mid + 1; else { minid = mid; high = mid - 1; }
+                }
+                if (target < nums[minid] || target > nums[minid - 1]) return -1;
+                if (target >= nums[0]) { low = 0; high = minid - 1; } else { low = minid; high = nums.Length - 1; }
+            }
+            else
+            {
+                if (target > nums[^1] || target < nums[0]) return -1;
+            }
+
+            while (low <= high)
+            {
+                mid = low + ((high - low) >> 1);
+                switch (nums[mid] - target)
+                {
+                    case > 0: high = mid - 1; break;
+                    case < 0: low = mid + 1; break;
+                    default: return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- C#/LeetCode/QuestionBank/Question0033/Test0033.cs
+++ C#/LeetCode/QuestionBank/Question0033/Test0033.cs
@@ -10,39 +10,57 @@ namespace LeetCode.QuestionBank.Question0033
     {
         public void Test()
         {
-            Interface0033 solution = new Solution0033_2();
+            Interface0033 solution = new Solution0033_3();
             int[] nums; int target;
             int result, answer;
             int id = 0;
 
             // 1. 
-            nums = new int[] { 4, 5, 6, 7, 0, 1, 2 }; target = 0;
-            answer = 4; result = solution.Search(nums, target);
+            nums = [4, 5, 6, 7, 0, 1, 2]; target = 0;
+            answer = 4;
+            result = solution.Search(nums, target);
             Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
 
             // 2. 
-            nums = new int[] { 4, 5, 6, 7, 0, 1, 2 }; target = 3;
-            answer = -1; result = solution.Search(nums, target);
+            nums = [4, 5, 6, 7, 0, 1, 2]; target = 3;
+            answer = -1;
+            result = solution.Search(nums, target);
             Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
 
             // 3. 
-            nums = new int[] { 4, 5, 6, 7, 8, 9, 0, 1, 2 }; target = 5;
-            answer = 1; result = solution.Search(nums, target);
+            nums = [4, 5, 6, 7, 8, 9, 0, 1, 2]; target = 5;
+            answer = 1;
+            result = solution.Search(nums, target);
             Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
 
             // 4. 
-            nums = new int[] { 1 }; target = 0;
-            answer = -1; result = solution.Search(nums, target);
+            nums = [1]; target = 0;
+            answer = -1;
+            result = solution.Search(nums, target);
             Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
 
             // 5. 
-            nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; target = 5;
-            answer = 4; result = solution.Search(nums, target);
+            nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]; target = 5;
+            answer = 4;
+            result = solution.Search(nums, target);
             Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
 
             // 6. 
-            nums = new int[] { 5, 1, 3 }; target = 0;
-            answer = -1; result = solution.Search(nums, target);
+            nums = [5, 1, 3]; target = 0;
+            answer = -1;
+            result = solution.Search(nums, target);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 7. 
+            nums = [1, 3]; target = 0;
+            answer = -1;
+            result = solution.Search(nums, target);
+            Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
+
+            // 8. 
+            nums = [3, 1]; target = 1;
+            answer = 1;
+            result = solution.Search(nums, target);
             Console.WriteLine($"{++id,2}: {(result == answer) + ",",-6} result: {result}, answer: {answer}");
         }
     }
