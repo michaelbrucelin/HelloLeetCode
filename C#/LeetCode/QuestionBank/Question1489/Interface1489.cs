c24cd3af2d8a4cedd25e47c7f76d393e51946fe4
michaelbrucelin <michaelbrucelin@gmail.com>
2026-07-08 10:14:16 +0800

    20260708-101415

---
+++ C#/LeetCode/QuestionBank/Question1489/Interface1489.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1489
+{
+    public interface Interface1489
+    {
+        public IList<IList<int>> FindCriticalAndPseudoCriticalEdges(int n, int[][] edges);
+    }
+}
