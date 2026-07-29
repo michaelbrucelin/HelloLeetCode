a885b9da4bb3cac57aadb6ca901d795e7d0a5672
michaelbrucelin <michaelbrucelin@gmail.com>
2026-07-29 09:24:37 +0800

    20260729-092437

---
+++ C#/LeetCode/QuestionBank/Question1248/Solution1248.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1248
+{
+    public class Solution1248 : Interface1248
+    {
+        /// <summary>
+        /// 滑动窗口 + 贡献法
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int NumberOfSubarrays(int[] nums, int k)
+        {
+            int result = 0, cnt = 0, p1 = 0, p2 = -1, len = nums.Length;
+
+            return result;
+        }
+    }
+}
