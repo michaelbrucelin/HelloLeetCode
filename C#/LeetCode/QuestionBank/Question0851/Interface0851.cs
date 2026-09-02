535e743445a9505516cd0652cc4a83e3f5eb5f8e
michaelbrucelin <michaelbrucelin@gmail.com>
2026-09-03 07:40:46 +0800

    20260903-074045

---
+++ C#/LeetCode/QuestionBank/Question0851/Interface0851.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0851
+{
+    public interface Interface0851
+    {
+        public int[] LoudAndRich(int[][] richer, int[] quiet);
+    }
+}
